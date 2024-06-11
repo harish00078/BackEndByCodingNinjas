3d87e62b06c01028b05b5a113abf123c28a2da4b
harish kumar <harishpathania150@gmail.com>
2024-06-11 21:19:27 +0530

    learn to create server in nodejs using http module

---
+++ Lecture1-GettingStartedWithNodeJs/Topic9-CreatingAnHttpServer/code/script.js
@@ -0,0 +1,19 @@
+// Here we learn about creating HTTP-server in node.js:
+// => 1 = first we need to import the (http) module from the nodejs.it is built in library or module in nodejs:
+// -> through which we are gonna be able to initiate the communication-protocal on our server application:
+// -> with the help of that clients are gonna be able to access and work with our server:
+// IMP =  for importing some thing in node.js:we use the (require) function:it is one of way to import something:
+// this require function basically return us that module.and for using it we have to store it in the variable
+const http = require('http');
+
+// => 2 = second we need to create-server:so for creating server we need to use the http-module:only because of that we are gonna be able to create the server:
+// IMP = so creating server using http-module.we have to access its (createServer) method:with the help of that method we are gonna be able to create the simple http-server:
+// V.IMP = this method takes one parameter as an argument:and that is a callback function.this callback function is basically a (request-listener):which help us to get requests from the client:and return back responses to them:acc to the requests that we get from the client.
+// this argument that will be called every time a request is made to the server. The callback function has two arguments: (req), which represents the incoming HTTP request, and (res), the HTTP-response-object that we can use to send data back to the client.
+// V.V.IMP = this createServer method or function basically return us the (instance) of our (server):which we have stored in the (server) variable: so that we can run our server by using its instance:
+// Instance of Server:The instance of server in Node.js is an object that represents the server. It has properties and methods that allow you to configure and control the server.
+
+const server = http.createServer((req,res)=>{
+    // response has (end) method in it:which basically stop the request response cycle of the paritcular request:and sends response back to the client:acc to its request:
+    res.end('hey! every one welcome to the NodeJs Server')
+})
