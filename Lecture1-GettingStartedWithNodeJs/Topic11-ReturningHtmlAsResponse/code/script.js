8459e569ef8ef485665aa013294804ecf6d56930
harish kumar <harishpathania150@gmail.com>
2024-06-21 11:45:59 +0530

    learn to return html as response

---
+++ Lecture1-GettingStartedWithNodeJs/Topic11-ReturningHtmlAsResponse/code/script.js
@@ -0,0 +1,15 @@
+// Learn to return HTML as a response:
+const http = require("http");
+// IMP = here we are importing the (fs) module or libraray:it is a built-in module in nodejs:which known as (file-system) module:using this module we can access or work with files on our system:
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  // we need to use the (readFileSync) method of (fs) module:To read or access the file from the system:
+  const data = fs.readFileSync("./index.html");
+  res.end(data);
+});
+
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`Server is up and running:${PORT}`);
+});
