b86ae77d06b0034eeef2b3dcc88d2da70e52901f
harish kumar <harishpathania150@gmail.com>
2024-06-25 11:37:28 +0530

    learn about modules in nodejs

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
@@ -1,9 +1,71 @@
 // here we gonna see that:how Modules are help full in Nodejs:
-// for that here we gonna create the two arithametic functions:both of these arithametic functions gonna get the same type of (argument) values:
+// for that here we gonna create the two arithametic functions:both of these arithametic functions gonna get the same type of (argument) variables:
 
-function sum(x,y){
-    return x+y;
+function sum(x, y) {
+  return x + y;
 }
-function div(x,y){
-    return x/y;
+function div(x, y) {
+  return x / y;
+}
+
+ 
+//V.IMP = we can also use the modules directly with the functions and with others things as well:
+
+// => first =  we see that how we can do that with the help of (commonjs) modules:
+// V.IMP:here we use (exports) keyword:
+
+// IMP = we can create its function in two ways:
+// -> 1 =  create function using function-keyword:
+// module.exports = function sum(x, y) {
+//   return x + y;
+// }
+
+// -> 2 = more simpler way is that we can use the arrow-functions:
+// module.exports.sum = (x, y) => {
+//     return x + y;
+// }
+
+// => second =  we see that how we can do that with the help of (es6) modules:
+// V.IMP = here we use (export) keyword:
+
+// IMP = we can create its functions in two ways:
+// => 1 =  create function using function-keyword:
+export function divide(x, y) {
+    return x / y;
+}
+
+// => 2 = more simpler way is that we can use the arrow-functions:
+export const divide = (x, y) => {
+    return x / y;
+}
+
+
+// IMP = using modules we can share our code.or we can say that the particular section of our code from file to another:with the help of modules we can use something on the other file which we have created on this file:
+// IMP = we have two ways to do that:these are not types they are only the ways to create a module:we can also call them two diff syntaxes to create modules in nodejs:
+
+// => CommonJs Module:CommonJS is the default module system in Node.js that uses the ‘require’ function to import modules and the ‘module.exports’ object to export them:
+// -> we can also do it in two ways:
+// -> first = is that we can do it as key-value pair with in the module.exports object:
+module.exports = {
+  add: sum,
+  Div: div,
+};
+// -> second = is that we can simply proivde our created functions as keys with in the module.exports object:
+// module.exports = {
+//   sum,
+//   div,
+// };
+
+// => ES6 Module:The ES6 module syntax is a more modern approach that is supported by modern JavaScript environments, and it employs the "import" and "export" keywords:
+// V.IMP = For this approach we did not have to create the module.exports object:we can simply export our function or module by using the (export) keyword in front of them:
+
+// IMP = we can create its functions in two ways:
+// -> 1 =  create function using function-keyword:
+export function divide(x, y) {
+    return x / y;
+}
+
+// -> 2 = more simpler way is that we can use the arrow-functions:
+export const divide = (x, y) => {
+    return x / y;
 }
--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
@@ -1,1 +1,17 @@
-// here we  gonna use those arithametical functions:that we have created by our self:
+// here we are gonna use those arithametical functions modules:and get the result from them and print it on the terminal:
+
+// V.IMP => first we gonna see that how we can import things which are exported by the CommonJs-based modules:
+
+// V.V.IMP = here we do it with the help of (require) method:
+// we can also do it in two ways:
+// V.V.IMP = when we are calling our module functions here they are respersenting there selfs as properties:because of the module.exports object:because we are exporting those functions with in the object:
+
+// 1 = first is that we can import the hole-file:and proivde him the variable-name:and using that varaible-name.we can use our modules by calling them through the vairble-name:whenever we want to use them:
+
+// const arithameticModules = require('./arithametic');
+
+// 2 = second is that we can only import the modules that we want to use from the file:
+const {add,Div} = require('./arithametic');
+
+console.log(add(3, 0));
+console.log(Div(9, 40));
