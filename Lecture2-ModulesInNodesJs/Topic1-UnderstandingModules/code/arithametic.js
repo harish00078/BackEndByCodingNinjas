fd62d124871065f67356237a56bcbb923e5530ce
harish kumar <harishpathania150@gmail.com>
2024-06-25 08:59:12 +0530

    learn about modules in node js

---
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
@@ -0,0 +1,9 @@
+// here we gonna see that:how Modules are help full in Nodejs:
+// for that here we gonna create the two arithametic functions:both of these arithametic functions gonna get the same type of (argument) values:
+
+function sum(x,y){
+    return x+y;
+}
+function div(x,y){
+    return x/y;
+}
---
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
@@ -0,0 +1,1 @@
+// here we  gonna use those arithametical functions:that we have created by our self:
