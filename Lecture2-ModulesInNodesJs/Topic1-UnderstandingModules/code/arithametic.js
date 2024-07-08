6d5f572839e51256fc8f7627773165d6651997cc
harish kumar <harishpathania150@gmail.com>
2024-07-08 20:14:06 +0530

    learn about modules

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
@@ -1,61 +1,63 @@
 // here we gonna see that:how Modules are help full in Nodejs:
+
+// Modules help us to do mainly two things:
+// => first =  using modules we can share our code.or we can say that the particular section of our code from one-file to another-file:with the help of modules we can use something on the other file which we have created on the one particular file:
+// => Secondly = V.V.IMP(Note)= In Node.js, with the help of modules, we are able to connect our code or its multiple files:within one file and that one file is basically connected to our main app-file or server-file:We can also connect those files one by one with the main server-file. Instead of combining them first into one file:this is how we connect multiple files with in our Node.js or server application.
+
+// IMP => we have two ways to do that:these are not types they are only the ways to create a module:we can also call them two diff syntaxes to create modules in nodejs:
+// -> 1 = CommonJs Module syntax:
+// -> 2 = ES6 Module syntax:
+
 // for that here we gonna create the two arithametic functions:both of these arithametic functions gonna get the same type of (argument) variables:
 
 function sum(x, y) {
   return x + y;
 }
+
 function div(x, y) {
   return x / y;
 }
 
-//V.IMP = we can also use the modules directly with the functions and with others things as well:
+//V.IMP(NOTE) = here we see that be can also use the modules directly with the functions and with others things as well:
 
-// => first =  we see that how we can do that with the help of (commonjs) modules:
-// V.IMP:here we use (exports) keyword:
+// => first = here we see that how we can do that with the help of (commonjs) modules method:
+// In commonjs modules method we use (module.exports) keyword:it is basically a (object):In which we assign our function,variables,or anything that we want to export from our file:as a key-value pair:same thing we do with the objects:
+// V.IMP(NOTE):here we use (exports) keyword:
 
-// IMP = we can create its function in two ways:
+// V.IMP = we use it with both function types:(simple-functions) and (arrow-functions):
+
 // -> 1 =  create function using function-keyword:
-// module.exports = function sum(x, y) {
-//   return x + y;
-// }
 
-// -> 2 = more simpler way is that we can use the arrow-functions:
-// module.exports.sum = (x, y) => {
+// module.exports = function add(x, y) {
 //     return x + y;
-// }
-
-// => second =  we see that how we can do that with the help of (es6) modules:
-// V.IMP = here we use (export) keyword:
-
-// IMP = we can create its functions in two ways:
-// => 1 =  create function using function-keyword:
-export function divide(x, y) {
-  return x / y;
-}
-
-// => 2 = more simpler way is that we can use the arrow-functions:
-export const divide = (x, y) => {
-  return x / y;
-};
-
-// IMP = using modules we can share our code.or we can say that the particular section of our code from file to another:with the help of modules we can use something on the other file which we have created on this file:
-// IMP = we have two ways to do that:these are not types they are only the ways to create a module:we can also call them two diff syntaxes to create modules in nodejs:
-
-// => CommonJs Module:CommonJS is the default module system in Node.js that uses the ‘require’ function to import modules and the ‘module.exports’ object to export them:
-// -> we can also do it in two ways:
-// -> first = is that we can do it as key-value pair with in the module.exports object:
-module.exports = {
-  add: sum,
-  Div: div,
-};
-// -> second = is that we can simply proivde our created functions as keys with in the module.exports object:
-// module.exports = {
-//   sum,
-//   div,
 // };
 
+// -> 2 = more simpler way is that we can use the arrow-functions:
+
+// module.exports.multiple = (x, y) => {
+//     return x * y;
+// };
+
+// => CommonJs Module:CommonJS is the default module system in Node.js that uses the ‘require’ function to import modules and the ‘module.exports’ object to export them:
+
+// => we can also do it in two ways:
+// -> first = is that we can do it as key-value pair with in the module.exports object:
+// module.exports = {
+//   add: sum,
+//   Div: div,
+// };
+// -> second = is that we can simply proivde our created functions as keys with in the module.exports object:
+module.exports = {
+  sum,
+  div,
+};
+
+
+// => second =  here we see that how we can do that with the help of (ES6) modules:
+// V.IMP = here we use (export) keyword:
+
 // => ES6 Module:The ES6 module syntax is a more modern approach that is supported by modern JavaScript environments, and it employs the "import" and "export" keywords:
-// V.IMP = For this approach we did not have to create the module.exports object:we can simply export our function or module by using the (export) keyword in front of them:
+// V.IMP = For this approach we did not have to create the module.exports object:we can simply export our function or module by using the (export) keyword in front of them or after them:
 
 // IMP = we can create its functions in two ways:
 // -> 1 =  create function using function-keyword:
--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
@@ -3,15 +3,16 @@
 // V.IMP => first we gonna see that how we can import things which are exported by the CommonJs-based modules:
 
 // V.V.IMP = here we do it with the help of (require) method:
-// we can also do it in two ways:
-// V.V.IMP = when we are calling our module functions here they are respersenting there selfs as properties:because of the module.exports object:because we are exporting those functions with in the object:
+// V.V.IMP(NOTE) = here we can see that. when we are calling our module functions here they are respersenting there selfs as properties:because of the module.exports object:because we are exporting those functions with in the object:
 
-// 1 = first is that we can import the hole-file:and proivde him the variable-name:and using that varaible-name.we can use our modules by calling them through the vairble-name:whenever we want to use them:
+// we can also do it in two ways:
+// 1 = first is that we can import the entire file into a variable-name and use it to access our modules or its module-functions:
 
 // const arithameticModules = require('./arithametic');
 
-// 2 = second is that we can only import the modules that we want to use from the file:
-const {add,Div} = require('./arithametic');
-
-console.log(add(3, 0));
-console.log(Div(9, 40));
+// 2 = second is that we can only import the modules direclty that we want to use from the file:
+// const {div,sum} = require('./arithametic');
+const {multiple} = require('./arithametic');
+console.log(multiple(3, 7));
+// console.log(div(9, 40));
+// console.log(sum(9,5));
