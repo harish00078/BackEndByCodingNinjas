86dbfb1a1b87e215735de2ea9a85855544e744a8
harish kumar <harishpathania150@gmail.com>
2024-07-18 18:44:10 +0530

    learn about modules in nodejs

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
@@ -3,7 +3,7 @@
 // V.IMP => first we gonna see that how we can import things which are exported by the CommonJs-based modules:
 
 // V.V.IMP = here we do it with the help of (require) method:
-// V.V.IMP(NOTE) = here we can see that. when we are calling our module functions here they are respersenting there selfs as properties:because of the module.exports object:because we are exporting those functions with in the object:
+// V.V.IMP(NOTE) = here we can see that. when we are calling our module functions here they are respersenting there selfs as (properties):because of the module.exports object:because we are exporting those functions with in the object:
 
 // we can also do it in two ways:
 // 1 = first is that we can import the entire file into a variable-name and use it to access our modules or its module-functions:
@@ -12,7 +12,7 @@
 
 // 2 = second is that we can only import the modules direclty that we want to use from the file:
 // const {div,sum} = require('./arithametic');
-const {multiple} = require('./arithametic');
-console.log(multiple(3, 7));
-// console.log(div(9, 40));
-// console.log(sum(9,5));
+const  = require("./arithametic");
+
+console.log(add(9, 40));
+console.log(sum(9, 5));
