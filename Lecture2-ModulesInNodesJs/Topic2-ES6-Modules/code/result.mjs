5ae0e2ce7438ea8ea5c3511a783da2fe4a5cf1e5
harish kumar <harishpathania150@gmail.com>
2024-07-30 18:12:01 +0530

    updated the code

--- Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/result.mjs
+++ Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/result.mjs
@@ -9,6 +9,7 @@
 // => 1 = first is that we can use the asterisk- keyword (*):To import all modules at once:and store them in the variable-name:
 import * as arithameticModules from "./script.mjs";
 console.log(typeof (arithameticModules.multiple));
+
 // => 2 = second is that we can import the hole-file:and store it in the variable:and from that variable-name we can access our module functions or other things those we have exported:and access them by using the dot (.) keyword on the variable-name:
 
 // => 3 = third way is that we can only import those modules which we want to use from the  exported-file:for doing this basically we need to import the modules using (require) method:and put in the (object) by using currly braces:
