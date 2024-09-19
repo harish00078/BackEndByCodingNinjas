a167f28635d62035e3a079a9927e0041b973ac07
harish kumar <harishpathania150@gmail.com>
2024-09-19 11:16:14 +0530

    updated comment section

--- Lecture1-GettingStartedWithNodeJs/Topic11-ReturningHtmlAsResponse/code/script.js
+++ Lecture1-GettingStartedWithNodeJs/Topic11-ReturningHtmlAsResponse/code/script.js
@@ -2,7 +2,7 @@
 const http = require("http");
 // IMP = here we are importing the (fs) module or libraray:it is a built-in module in nodejs:which known as (file-system) module:using this module we can access or work with files on our system:
 const fs = require("fs");
-// IMP = here we are creating a server using the http module:
+// IMP = here we are creating a server using the http module:we are using the (createServer) method of (http) module:
 const server = http.createServer((req, res) => {
   // we need to use the (readFileSync) method of (fs) module:To read or access the file from the system:
   const data = fs.readFileSync("./index.html");
