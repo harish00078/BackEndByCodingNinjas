e114782550173ea15ac814e3c8f66e2094217587
harish kumar <harishpathania150@gmail.com>
2024-10-13 10:00:19 +0530

    updated

--- Lecture1-GettingStartedWithNodeJs/Topic10-HandlingMultipleRequests/code/script.js
+++ Lecture1-GettingStartedWithNodeJs/Topic10-HandlingMultipleRequests/code/script.js
@@ -16,12 +16,12 @@ const server = http.createServer((req, res) => {
     // IMP = we also need to use the (return) statement with the (responses) end-method:so that our responses (execution-functions) did not crash with each other:
 
     // NOTE => (IMP-points):
-    // => 1 = it happens because.when we are ending the request.then only the request gets ending not its execution-function:so we also have to end its execution-function.because we did not want the two execution-function of the end-method did not get crash with each other:
-    // (v.IMP)=> 2 = other things is that if after sending  response and ending the request:we wanna do more changes on that request or its response.then we  have to avoid using the return statement on end method:
+    // => 1 = it happens because.when we are ending the request.then only the request gets ending not its execution-function:so we also have to end its execution-function.because we did not want the two execution-function of the end-method did  get crash with each other:
+    // (V.V.IMP)=> 2 = other things is that if after sending  response and ending the request:we wanna do more work on that request or its response.then we  have to avoid using the return statement on end method:
 
     // NOTE = (Execution-Functions): An execution context in JavaScript is the environment in which code is executed. There are different kinds of execution contexts, but the two most common are:
-    // => 1 = Global Execution Context: This is the default context in which the code that is not inside any function runs. In a browser, it refers to the global window object.
-    // => 2 = Function Execution Context: Each time a function is called, a new execution context is created for that function.
+    // => 1 = Global Execution Context: This is the default context in which the code that is not inside any function runs. In a browser, it refers to the global window object:
+    // => 2 = Function Execution Context: Each time a function is called, a new execution context is created for that function:
 
     return res.end("Products! that we have");
   } else if (req.url == "/user") {
