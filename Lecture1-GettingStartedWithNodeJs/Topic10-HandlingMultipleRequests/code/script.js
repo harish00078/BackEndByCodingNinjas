171f4b21f5ceaa5b6cd0318ee16e870451cb1626
harish kumar <harishpathania150@gmail.com>
2024-10-13 10:24:27 +0530

    updated

--- Lecture1-GettingStartedWithNodeJs/Topic10-HandlingMultipleRequests/code/script.js
+++ Lecture1-GettingStartedWithNodeJs/Topic10-HandlingMultipleRequests/code/script.js
@@ -18,6 +18,7 @@ const server = http.createServer((req, res) => {
     // NOTE => (IMP-points):
     // => 1 = it happens because.when we are ending the request.then only the request gets ending not its execution-function:so we also have to end its execution-function.because we did not want the two execution-function of the end-method did  get crash with each other:
     // (V.V.IMP)=> 2 = other things is that if after sending  response and ending the request:we wanna do more work on that request or its response.then we  have to avoid using the return statement on end method:
+    // For-Example:if wanna do some (logging) or other things with those particular request's response:
 
     // NOTE = (Execution-Functions): An execution context in JavaScript is the environment in which code is executed. There are different kinds of execution contexts, but the two most common are:
     // => 1 = Global Execution Context: This is the default context in which the code that is not inside any function runs. In a browser, it refers to the global window object:
