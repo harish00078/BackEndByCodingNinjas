2de2e0a8fe1ebe20d15cddde0b4e8edddff7dcb2
harish kumar <harishpathania150@gmail.com>
2024-10-18 18:41:56 +0530

    learning about modules in nodejs

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
@@ -26,11 +26,11 @@
 // In commonjs modules method or syntax way we use (module.exports) keywords or (object) to (export) things:
 // V.IMP(NOTE):It is basically uses a (object) that's why it receives or have a (object) with in it:In which we assign our function,variables,or anything that we want to export from our file:
 // IMP(NOTE): so its gonna export them as (key-value) pairs:same thing that we  have with in the simple-objects:
-// // V.V.V.IMP(NOTE):In CommonJs modules method we use (exports) keyword:because here In this syntax we can export mulitple things at one time:
 
 
-// => CommonJs Module:CommonJS is the default module system in Node.js that uses the ‘require’ function to import modules and the ‘module.exports’ object to export them: 
-// V.IMP-NOTE = here these functions gonna be exports as properties:with in properties the (functions) become's the (methods):
+// => CommonJs Module:CommonJS is the (default) module-system in Node.js which uses the ‘require’ function to import modules and the ‘module.exports’ object to export them: 
+// V.IMP-NOTE = here these functions gonna be exports as properties:And as we also know that if we export (functions) as properties then those (functions) gonna become (methods):
+
 // V.IMP(NOTE)=> we can also use module.exports object in two ways:
 // => [first-way]:with in this first-way: we also have the mulitple ways do it as well:
 // -> first = is that we can do it as key-value pair with in the module.exports object:
