596f61932c08ad7142aab5bd8d99f30bb577b558
harish kumar <harishpathania150@gmail.com>
2024-10-19 18:42:20 +0530

    learning about modules in nodejs

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
@@ -23,9 +23,9 @@
 //   return x / y;
 // }
 
-// In commonjs modules method or syntax way we use (module.exports) keywords with  (object) or without the (object) to (export) things:acc to that how many of things we want to export:
+// V.V.IMP: In commonjs modules method or syntax way we use (module.exports) keywords with  (object) or without the (object) to (export) things:we use them according to how many of things we want to export:
 // V.IMP(NOTE):CommonJs basically uses a (object) to (export) things that's why it receives or have a (object) with in it:In which we assign our function,variables,or anything that we want to export from our file:
-// IMP(NOTE): And that's why its gonna export them as (key-value) pairs:same thing that we  have with in the simple-objects:
+// IMP(NOTE): And that's why its gonna be iexport them as (key-value) pairs or (Properties):same thing that we  have with in the simple-objects:
 
 
 // => CommonJs Module:CommonJS is the (default) module-system in Node.js which uses the (require) function to import-modules and the (module.exports) object to export them: 
