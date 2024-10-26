40530c6331238534adc226e444e8426157abf945
harish kumar <harishpathania150@gmail.com>
2024-10-26 10:44:24 +0530

    learning about modules in nodejs

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
@@ -30,7 +30,9 @@
 // => CommonJs Module:CommonJS is the (default) module-system in Node.js which uses the (require) function to import-modules and the (module.exports) object to export them:
 // V.IMP-NOTE = here these functions gonna be exports as properties because of the (object) usage:and we have also already know that if we export (functions) as properties then those (functions) become's (methods):
 
-// V.IMP(NOTE)=> we can also use (module.exports) object or (commonjs-export) syntax in two ways:
+// V.IMP(NOTE)=> we can also use (module.exports) object or (commonjs-export) syntax in three and more ways:but the most common once are:
+// -> 1 = use (module.exports) keyword with the help of (objects):
+// -> 2 = use (module.exports) keywords with the help of (objects):
 
 // => [first-way]:Is with the help of (objects):with in this first-way:we also have the multiple ways do it as well:mainly its in two-ways:
 
@@ -77,7 +79,9 @@
 //   return x * y;
 // }
 
-// -> 3 = more simpler way is that we can use the arrow-functions:
+// [Second-type]: Arrow-Functions:means those function which we are not created using the (function) keywords:they are created using the (arrow-functions):
+// IMP = here we are using the (arrow-functions) directly:And we declare (arrow-functions) using (equalTo(=)) and (greaterThan(>)) sign combine:
+
 // V.IMP-NOTE = here this function is also gonna be export as properties:
 
 // module.exports.multiple = (x, y) => {
