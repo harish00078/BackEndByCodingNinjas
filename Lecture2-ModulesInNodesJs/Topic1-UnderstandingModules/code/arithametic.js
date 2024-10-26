2f7376aadfb495e7c0f5d562ac4cee7b5232e03f
harish kumar <harishpathania150@gmail.com>
2024-10-26 10:47:44 +0530

    learning about modules in nodejs

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
@@ -30,9 +30,10 @@
 // => CommonJs Module:CommonJS is the (default) module-system in Node.js which uses the (require) function to import-modules and the (module.exports) object to export them:
 // V.IMP-NOTE = here these functions gonna be exports as properties because of the (object) usage:and we have also already know that if we export (functions) as properties then those (functions) become's (methods):
 
-// V.IMP(NOTE)=> we can also use (module.exports) object or (commonjs-export) syntax in three and more ways:but the most common once are:
+// V.IMP(NOTE)=> we can also use (module.exports) object or (commonjs-export) syntax in (three) and more ways:but the most common once are:
 // -> 1 = use (module.exports) keyword with the help of (objects):
-// -> 2 = use (module.exports) keywords with the help of (objects):
+// -> 2 = use (module.exports) keywords without the help of (objects):
+// -> 3 = use only (exports) keywords with the help of (objects) and without (objects):
 
 // => [first-way]:Is with the help of (objects):with in this first-way:we also have the multiple ways do it as well:mainly its in two-ways:
 
@@ -88,6 +89,7 @@
 //     return x * y;
 // };
 
+// => [Third-way]: we can also do it only with the help of (exports) keyword:
 // V.V.IMP = here we see that we can also use the (exports) object directly with the functions and with others things as well:
 
 // IMP(NOTE) = 2 => (this method specifically export the functions or anything as properties):
