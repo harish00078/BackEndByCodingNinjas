4d2d23494ab298dac10c251a404077f449ca2b2c
harish kumar <harishpathania150@gmail.com>
2024-11-09 19:01:00 +0530

    learning about modules in nodejs

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithametic.js
@@ -133,8 +133,7 @@
 
 // exports.sum = sum;
 
-// (V.IMP-NOTE)-[Commonjs-Syntax] :- If we export something (like a function) and import it in another file:
-
+// (V.IMP-NOTE)-[Commonjs-Syntax] :- If we export something (like a function) and import it in another file:then that function will be available in the importing file:
 // V.V.IMP-[NOTE]: Then that entire file gets executed once from where we are exporting that some-thing:
 // V.V.IMP: later the imports use that already executed code. But only the specific parts that we import will be accessible in the importing file. This is little different in ES6 modules, which can eliminate unused code.because of its (tree-shaking) approach:
 
