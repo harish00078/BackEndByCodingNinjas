eaabc0656b3a4a5292416931bc8ffadf2dee4e0d
harish kumar <harishpathania150@gmail.com>
2024-11-20 20:31:04 +0530

    updated the comment

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
@@ -15,7 +15,6 @@
 // V.IMP-NOTE = if we export some thing as (functions) then we have to use the second-way here to import things:
 
 // [first-way] = In first way.we have two types to import the things:
-
 // => 1 =  first is that we can import the hole-file:and store it in the variable:and from that variable-name we can access our module functions or other things those we have exported:and access them by using the dot (.) keyword on the variable-name:
 // => 2 = second way is that we can only import those modules which we want to use from the  exported-file:for doing this basically we need to import the modules using (require) method:and put it in the (object) by using curly braces:
 
