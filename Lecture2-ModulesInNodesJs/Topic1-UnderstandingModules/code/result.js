7a205be44c456e1cc5ed691aa608a50f3106cb94
harish kumar <harishpathania150@gmail.com>
2024-11-28 20:13:17 +0530

    updated the comment

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
@@ -10,6 +10,7 @@
 
 // -> [first-way]:(Explanation):In this way we basically (exports) in two types:
 // -> [1-type]: first is that using the default export syntax of commonjs syntax:and that is (module.exports) object:
+
 // => 1 = if we are using the (module.exports) object:and assigning our functions as (key-value) pairs or (properties) to it:then we use the first-way here to import things: 
 // -> [2-type]:second is that simply using the (export) keyword:and that is (exports) object: 
 // with in this type:if we are using the (exports) keyword directly:and assigning our functions as properties to it:then we also have to use the first-way here as well to import things:
