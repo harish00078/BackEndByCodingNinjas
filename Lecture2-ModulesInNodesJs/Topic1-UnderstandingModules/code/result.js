d9386038c1adec268f72a46a4397f4bb546d3a71
harish kumar <harishpathania150@gmail.com>
2024-11-30 18:38:54 +0530

    learn about importing through commonjs

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
@@ -3,7 +3,7 @@
 // ===>V.V.IMP: here we gonna learn about that:how be (Import) things in (commonjs) modules syntax: acc to its exports or exporting-ways:
 // V.V.IMP = Here In (commonjs):we (Import) things with the help of (require) method:
 // IMP-NOTE: when we say that.we are importing the all modules at once:it basically means that we are only importing the modules which we have exported:and not importing any other modules:
-// V.V.IMP(NOTE) = when we (call) or (Import) our (module-functions) which we have created:they represent there selfs as (properties):because of the module.exports object:because we are exporting those functions through the object:
+// V.V.IMP(NOTE) = when we (call) or (Import) our (module-functions) which we have created and exported:they represent there selfs as (properties):because of the module.exports object:because we are exporting those functions through the object:
 
 // => V.IMP = Here through (commonjs) syntax we can import the modules in two-ways:It mostly depend on that how we are exporting things:
 // => IMP-NOTES:mostly we have Three ways to export things:and how be gonna import them using those two ways of importing:
