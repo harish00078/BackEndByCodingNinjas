5c87767dc3af335ed764a98111cf98cf62ab747c
harish kumar <harishpathania150@gmail.com>
2024-12-02 19:06:52 +0530

    learn about importing through commonjs

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
@@ -8,12 +8,12 @@
 // => V.IMP = Here through (commonjs) syntax we can import the modules in two-ways:It mostly depend on that how we are exporting things:
 // => IMP-NOTES:basically we have two ways of importing things:and three ways to export things in commonjs syntax:so here we gonna learn about that:how we can use those two ways of (imports) with the three-ways of (exports):
 
-// NOTE:These are the three ways of exporting things:
-// [first-way] = if we export some thing using (objects)with the module.exports keyword:which means that if we are assigning our exported things as (properties) or (key-value) to the objects:then we have to use the (first-way) to import things:
+// NOTE: These are the three ways of exporting things:
+// [first-way] = if we export some thing using (objects) with the module.exports keyword:which means that if we are assigning our exported things as (properties) or (key-value) to the objects:then we have to use the (first-way) to import things:
 // [second-way] = if we are directly using them with the (module.exports) keyword then we have to use the (second-way) to import things:
-// IMP-NOTE:With in second way of exporting things:we get two types of exporting things:or we can say that we get the two different types of export-results through this way:
+// IMP-NOTE:With in second way of exporting things:we get two types of exports:or we can say that we get the two-different types of export-results through the second-way of exporting things:
 // -> 1 = First its exports things like all other-ways:by simply exporting them as (properties) or (key-value) pairs:
-// -> 2(V.IMP) = Second it exports thing as they are:[for-example]:if we are exporting the (function):then its gonna be exported as (function):not as (properties) or (key-value) pairs like we have in in the other-ways:
+// -> 2(V.IMP) = Second it exports thing as they are:[for-example]:if we are exporting the (function):then its gonna be exported them as (function):not as (properties) or (key-value) pairs like we have in in the other-ways:
 // [third-way] = if we are directly using them with the (exports) keyword then we also have to use the (second-way) to import things:
 
 // NOTE:It basically a simple distribution:To use two-ways of (imports) on those three-ways of (exports):
@@ -21,6 +21,7 @@
 // => 1 = if we exporting things using (objects):then we use the (first-way) of importing things:
 // => 2 = if we exporting things directly with the exporting keyword:then we use the (second-way) of importing things:
 
+// Importing-Ways:
 // [first-way] = In first way:we have two types to import the things:
 // => 1(first-type) = Is that we can import the hole-file:and store it in the variable:and from that variable-name we can access our module functions or other things those we have exported:and access them by using the dot (.) keyword on the variable-name:
 // => 2(second-type) = Is that we can only import those modules which we want to use from the  exported-file:for doing this basically we need to import the modules using (require) method:and put it in the (object) by using curly braces:
