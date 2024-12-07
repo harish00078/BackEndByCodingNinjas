e78dd486aeab58d371333f2ec44040e0b9adc93f
harish kumar <harishpathania150@gmail.com>
2024-12-07 20:14:09 +0530

    learning about ES6 modules of nodejs

--- Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/result.mjs
+++ Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/result.mjs
@@ -9,10 +9,10 @@
 // => 1 = first:- is that we can use the asterisk- keyword (*):To import all modules at once:and store them in the variable-name:and from that variable-name we can access our module functions or other things those we have exported:and access them by using the dot (.) keyword on the variable-name:
 // V.IMP = all things are gonna we store with in the variable-name (object):
 // V.V.IMP-[NOTE]= [Tree shaking] is a term used in JavaScript for dead code elimination. It's a process that removes unused code from your final bundle, resulting in smaller and faster applications. Tree shaking relies heavily on the static structure of ES6 modules (import and export statements). This allows bundlers to analyze your code and determine which parts are actually used.
-import * as arithameticModules from "./script.mjs";
-console.log(arithameticModules.multiple(5, 5));
+import * as arithmeticModules from "./script.mjs";
+console.log(arithmeticModules.multiple(5, 5));
 
-// => 2 = second:- way is that we can only import those modules which we want to use from the exported-file:for doing this basically we need to import the modules using (import) method:and put it in the (object) by using currly braces:
+// => 2 = second:- way is that we can only import those modules which we want to use from the exported-file:for doing this basically we need to import the modules using (import) method:and put it in the (object) by using curly braces:
 
 import { sum } from "./script.mjs";
 console.log(sum(15, 5));
