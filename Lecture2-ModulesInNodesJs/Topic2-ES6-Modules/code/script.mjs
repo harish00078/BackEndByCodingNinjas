2a3c0e1b0cba1ffad80600732deb656c720d0402
harish kumar <harishpathania150@gmail.com>
2024-12-10 18:23:16 +0530

    learning about ES6 modules of  nodejs

--- Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/script.mjs
+++ Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/script.mjs
@@ -1,9 +1,10 @@
 // ==> In This:- Topic-file we are gonna be learn about (ES6 Module syntax):
 
-// V.V.IMP = Here in ES6-modules syntax we basically use the (export) keyword simply to export things or modules we can say from one file to another file:
+// V.V.IMP = In ES6-modules syntax we basically use the (export) keyword simply to export things or modules we can say from one file to another file:
 
-// => ES6 Module:The ES6 module syntax is a more modern approach that is supported by modern JavaScript environments, and it employs the "import" and "export" keywords:
+// => ES6 Module:The ES6 module syntax is a more modern approach that is supported by modern JavaScript environments or we can say by the (Libraries) and (Frameworks): and it uses the "import" and "export" keywords:
 // V.IMP = For this approach we did not have to create the module.exports object:we can simply export our function or anything that we want to export as a module by using the (export) keyword in front of them or after them:
+
 // V.IMP = this module-method.we have used in the (react):
 // V.V.IMP(NOTE) = so when we are using the (import) and (export) method:To use (Modules) from one file to another file in (plain-js) application:then we have to use the (mjs) extension in the file-name:instead of simple (js) extension.
 
