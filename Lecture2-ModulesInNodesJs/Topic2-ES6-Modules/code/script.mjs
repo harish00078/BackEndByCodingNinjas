27b6f794d1d0983f653d8c318ff25441f89cb7c6
harish kumar <harishpathania150@gmail.com>
2024-12-10 18:47:02 +0530

    learning about ES6 modules of  nodejs

--- Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/script.mjs
+++ Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/script.mjs
@@ -3,10 +3,11 @@
 // V.V.IMP = In ES6-modules syntax we basically use the (export) keyword simply to export things or modules we can say from one file to another file:
 
 // => ES6 Module:The ES6 module syntax is a more modern approach that is supported by modern JavaScript environments or we can say by the (Libraries) and (Frameworks): and it uses the "import" and "export" keywords:
-// V.IMP = For this approach we did not have to create the module.exports object:we can simply export our function or anything that we want to export as a module by using the (export) keyword in front of them or after them:
+// V.IMP = For this approach we did not have to create the module.exports object:we can simply export our function or anything that we want to export as a module by using the (export) keyword in (front) of them or (after) them:
+// V.IMP = we have use this (module) method or syntax.with in our (react) learnings:
 
-// V.IMP = this module-method.we have used in the (react):
-// V.V.IMP(NOTE) = so when we are using the (import) and (export) method:To use (Modules) from one file to another file in (plain-js) application:then we have to use the (mjs) extension in the file-name:instead of simple (js) extension.
+// V.V.IMP(NOTE) = so when we are using the (ES6) module-syntax:To (import) and (export) Modules from one file to another file:when we are mostly creating the application simply on the (nodejs) environment without the help of any framework or library:
+//then we have to use the (mjs) extension in the file-name:instead of simple (js) extension.
 
 // IMP = we can create its functions in two ways:
 // -> 1 =  create function using simple-function keyword:
