2887a86c77f300e6af83aec7e171a66b184baf88
harish kumar <harishpathania150@gmail.com>
2024-12-11 19:20:12 +0530

    learning about ES6 modules of  nodejs

--- Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/result.mjs
+++ Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/result.mjs
@@ -12,9 +12,9 @@
 // V.IMP = all things are gonna we store with in the variable-name (object):
 // V.V.IMP-[NOTE]= [Tree shaking] is a term used in JavaScript for dead code elimination. It's a process that removes unused code from your final bundle, resulting in smaller and faster applications. Tree shaking relies heavily on the static structure of ES6 modules (import and export statements). This allows bundlers to analyze your code and determine which parts are actually used.
 import * as arithmeticModules from "./script.mjs";
-console.log(arithmeticModules.multiple(5, 5));
+console.log(arithmeticModules.multiple(5,6));
 
 // => 2 = second:- way is that we can only import those modules which we want to use from the exported-file:for doing this basically we need to import the modules using (import) method:and put it in the (object) by using curly braces:
 
 import { sum } from "./script.mjs";
-console.log(sum(15, 5));
+console.log( sum(7,7));
