52b8e9776146ab4dceb8aec75053bc23b914e6c5
harish kumar <harishpathania150@gmail.com>
2024-12-13 19:54:44 +0530

    learning about ES6 modules of  nodejs

--- Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/result.mjs
+++ Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/result.mjs
@@ -9,15 +9,17 @@
 
 
 // => V.IMP: In (ES6) module syntax:we basically have three ways to import the things:
+// [First-way] = Is that we can import the all modules at once:
+// [Second-way] = Is that we can only import those modules which we want to use from the exported-file: 
 
-// [First-way]: Is that we can use the asterisk- keyword (*):To import all modules at once:and store them in the variable-name:and from that variable-name we can access our module functions or other things those we have exported:and access them by using the dot (.) keyword on the variable-name:
+// [First-way]: We use the asterisk- keyword (*):To import all modules at once:and store them in the variable-name:and from that variable-name we can access our module functions or other things those we have exported:and access them by using the dot (.) keyword on the variable-name:
 // V.IMP = all things are gonna we store with in the variable-name (object):
 
 
 import * as arithmeticModules from "./script.mjs";
 console.log(arithmeticModules.multiple(5, 6));
 
-// => 2 = second:- way is that we can only import those modules which we want to use from the exported-file:for doing this basically we need to import the modules using (import) method:and put it in the (object) by using curly braces:
+// [Second-way]: Is that we need to import the modules using (import) method:and put it in the (object) by using curly braces:
 
 import { sum } from "./script.mjs";
 console.log(sum(7, 7));
