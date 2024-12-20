1b078f3839d44ecc3c31381cd9eee897fac11d36
harish kumar <harishpathania150@gmail.com>
2024-12-20 18:46:20 +0530

    refactor: improve logging and formatting in script.js

--- Lecture2-ModulesInNodesJs/Topic6-UnderstandingPackage.JsonFiles/script.js
+++ Lecture2-ModulesInNodesJs/Topic6-UnderstandingPackage.JsonFiles/script.js
@@ -4,7 +4,9 @@ const server = http.createServer((req, res) => {
     res.end("understanding package.json-file");
 });
 
+console.log("understanding package.json-file");
+
 const PORT = 8000;
- server.listen(PORT, () => {
-     console.log(`Server listening on Port ${PORT}`);
- })
+server.listen(PORT, () => {
+    console.log(`Server listening on Port ${PORT}`);
+});
