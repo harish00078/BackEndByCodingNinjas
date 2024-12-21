440c5d41ea456dcda75581374d62a3c3421b340f
harish kumar <harishpathania150@gmail.com>
2024-12-21 10:08:52 +0530

    learn about package.json file

--- Lecture2-ModulesInNodesJs/Topic6-UnderstandingPackage.JsonFiles/script.js
+++ Lecture2-ModulesInNodesJs/Topic6-UnderstandingPackage.JsonFiles/script.js
@@ -1,12 +1,10 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.end("understanding package.json-file");
+  res.end("understanding package.json-file");
 });
-
-console.log("understanding package.json-file");
 
 const PORT = 8000;
 server.listen(PORT, () => {
-    console.log(`Server listening on Port ${PORT}`);
+  console.log(`Server listening on Port ${PORT}`);
 });
