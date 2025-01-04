a80517dadfec8e3cfc068f8eb8ba375f1e936044
harish kumar <harishpathania150@gmail.com>
2025-01-04 19:49:47 +0530

    learning about nvm

---
+++ Lecture2-ModulesInNodesJs/Topic9-UnderstandingNvm/script.js
@@ -0,0 +1,32 @@
+console.log("Understanding Node Version Manager: (NVM)");
+// Node Version Manager (NVM) is a tool that allows the management of multiple
+// Node.js versions on a computer. As a developer, managing multiple projects with
+// different Node.js versions can be difficult, but NVM helps to switch between them
+// easily, ensuring consistency and smooth operation.
+// How to use NVM
+// Installing NVM
+// 1. Open a terminal window.
+// 2. Install NVM using one of the following commands, depending on your operating
+// system:
+// ● For Mac:
+// 1. Install homebrew using:
+// /bin/bash -c "$(curl -fsSL
+// https://raw.githubusercontent.com/Homebrew/install/H
+// EAD/install.sh)
+// 2. Verify that homebrew is installed correctly by running brew
+// 3. Use brew install nvm to install nvm.
+// 4. Use source $(brew --prefix nvm)/nvm.sh to configure nvm
+// with the shell system.
+// ● For Windows: You can download and run the installer from the NVM
+// Windows repository on GitHub:
+// https://github.com/coreybutler/nvm-windows/releases
+// 3. Verify that NVM is installed correctly by running nvm --version
+// Some basic commands to use NVM:
+// 1. To list all the available Node.js versions:
+// nvm ls-remote
+// 2. To install a specific Node.js version:
+// nvm install <version>
+// 3. To switch to a different Node.js version:
+// nvm use <version>
+// 4. To set a default Node.js version to use in new shells:
+// nvm alias default <version></version>
