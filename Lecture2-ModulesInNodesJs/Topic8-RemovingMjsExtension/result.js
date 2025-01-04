489dad6b9d819e9ba2bdb84847fd25f658f7f859
harish kumar <harishpathania150@gmail.com>
2025-01-04 18:00:11 +0530

    learning about removing mjs extension

--- Lecture2-ModulesInNodesJs/Topic8-RemovingMjsExtension/result.js
+++ Lecture2-ModulesInNodesJs/Topic8-RemovingMjsExtension/result.js
@@ -1,3 +1,4 @@
-import * as Add from "./script.js";
-const result = Add.add(2, 3);
+// import * as Add from "./script.js";
+import{add} from "./script.js";
+const result = add(2, 3);
 console.log(result);
