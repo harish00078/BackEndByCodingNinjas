efeb1573b4654c080650e6d20a02b57945b08598
harish kumar <harishpathania150@gmail.com>
2025-01-06 17:18:32 +0530

    learn to read data from console

--- Lecture2-ModulesInNodesJs/Topic10-ReadingCommandLineInput/script.js
+++ Lecture2-ModulesInNodesJs/Topic10-ReadingCommandLineInput/script.js
@@ -8,6 +8,14 @@ const readline = require('readline');
 // => Step 2: Create an (Interface) using readline-module:and then configure that interface to read the data from the console or terminal:
 // IMP: for creating (interface):we have to use (createInterface) function of (readline) module:
 // Know we have two ways to configure an interface:
-// -> first-way: Is that we can directly use functions to configure it:
+// -> first-way: Is that we can directly use simple-functions to configure it:
 // -> second-way: Is that we can use the (options) object functions to configure it:
-readline.createInterface()
+readline.createInterface({
+    // IMP: (input) property is used to read the data from the console or terminal:Its gonna do that with the help of (process.stdin) object:
+    // where (process) is a global object in Node.js and (stdin) is a property of that object:which is used to read the data from the console or terminal:or we can say tell node.js to read the (input-data) from the console or terminal:
+    input: process.stdin,
+    // IMP: (output) property is used to write the data to the console or terminal:Its gonna do that with the help of (process.stdout) object:
+    // where (process) is a global object in Node.js and (stdout) is a property of that object:which is used to write the data to the console or terminal:
+    output:process.stdout
+    
+})
