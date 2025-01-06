5b0332fe2d8600081e24079a1016215c36ac8a7e
harish kumar <harishpathania150@gmail.com>
2025-01-06 17:04:51 +0530

    learn to read data from console

--- Lecture2-ModulesInNodesJs/Topic10-ReadingCommandLineInput/script.js
+++ Lecture2-ModulesInNodesJs/Topic10-ReadingCommandLineInput/script.js
@@ -5,5 +5,9 @@ console.log("Reading command line input AND reading Data from Console or Termina
 // => Step 1: Importing the 'readline' module from Node.js:
 const readline = require('readline');
 
-// => Step 2: Create an (Interface) using readline-module:and then configure it to read the data from the console or terminal:
-
+// => Step 2: Create an (Interface) using readline-module:and then configure that interface to read the data from the console or terminal:
+// IMP: for creating (interface):we have to use (createInterface) function of (readline) module:
+// Know we have two ways to configure an interface:
+// -> first-way: Is that we can directly use functions to configure it:
+// -> second-way: Is that we can use the (options) object functions to configure it:
+readline.createInterface()
