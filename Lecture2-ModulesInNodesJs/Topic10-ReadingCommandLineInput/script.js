af2422c3b7dad1e2391fbdfd5916f4a656a06336
harish kumar <harishpathania150@gmail.com>
2025-01-06 12:07:50 +0530

    learn to read data from console

--- Lecture2-ModulesInNodesJs/Topic10-ReadingCommandLineInput/script.js
+++ Lecture2-ModulesInNodesJs/Topic10-ReadingCommandLineInput/script.js
@@ -1,3 +1,6 @@
 console.log("Reading command line input AND reading Data from Console or Terminal in Node.js");
 
-// Steps we have to Follow:so that we can read command line input in Node.js:
+// Steps we have to Follow:so that we can read command line input or terminal-input in Node.js:
+
+// => Step 1: Importing the 'readline' module from Node.js:
+const readline = require('readline');
