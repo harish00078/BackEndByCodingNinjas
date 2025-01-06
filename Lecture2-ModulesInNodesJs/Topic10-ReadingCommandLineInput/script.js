273bc4625a7d0d3a87c06912d2e486a5e1b33512
harish kumar <harishpathania150@gmail.com>
2025-01-06 19:06:17 +0530

    learn to read data from console

--- Lecture2-ModulesInNodesJs/Topic10-ReadingCommandLineInput/script.js
+++ Lecture2-ModulesInNodesJs/Topic10-ReadingCommandLineInput/script.js
@@ -10,6 +10,7 @@ const readline = require("readline");
 // => Step-2: Create an (Interface) using readline-module:and then configure that interface to read the data from the console or terminal:
 // IMP: for creating (interface):we have to use (createInterface) function of (readline) module:
 // IMP-NOTE:this createInterface function is gonna return an object:which is gonna represent the interface:so we have to store that object or interface-object in a variable:
+// V.IMP-NOTE:the (createInterface) function help us to connect with the (console or terminal) and fetch or response the data to the console or terminal:
 // Know we have two ways to configure an interface:
 // -> first-way: Is that we can directly use simple-functions to configure it:
 // -> second-way: Is that we can use the (options) object functions to configure it:
@@ -33,6 +34,7 @@ const interface = readline.createInterface({
 // because we have configured that object to read or write the data of the console or terminal:with the help of it we can access the data of the console or terminal:which has been written by the user:
 // IMP: so here first we want to (read) the data from the console or terminal:or which has been provided by the user:
 // IMP-NOTE: we gonna do that with the help of (question) function of the interface-object:
+// V.IMP-NOTE:the (question) function basically help us to (reads) and (writes) the data to the console or terminal:with the help of the (interface-object)'s (input) and (output) properties:
 // => this function is gonna take three arguments:
 
 interface.question()
