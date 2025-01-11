4564fc1c4c4e8feda840f7e18fa6cd2b8b62bce8
harish kumar <harishpathania150@gmail.com>
2025-01-11 11:41:40 +0530

    learn about file system in nodejs

--- Lecture2-ModulesInNodesJs/Topic11-FileSystemModule1/script.js
+++ Lecture2-ModulesInNodesJs/Topic11-FileSystemModule1/script.js
@@ -1,11 +1,11 @@
 console.log("here we are learning about file-system-module in node js");
-// (FS)-File System Module:It basically allows us to work with the file system on our computer:
+// (FS)-File System Module:It basically allows us to work with the file system on our computer or machine:
 // V.IMP: We can use it to read, write, delete, update (data) in files and folders on our computer:It also help us to move our files and folders on the computer:
-// V.IMP-NOTE:Working with file-system is basically a (input-output) operation:because (fs) is also managed by the other system which is the operating system: so it is an input-output operation:because it has to interact with the operating system or we can say other system to perform the operations:
+// V.IMP-NOTE:Working with file-system is basically a (input-output) operation:because (fs) is also managed by the other system which is the operating system: so it is an input-output operation:because it has to interact with the operating system to perform the operations:
 // IMP-NOTE: It is a core module in node js, so we don't need to install it separately:
 
 // we have two-ways to use (fs) module in (nodejs):
-// -> 1 = synchronous-way
+// -> 1 = synchronous-way:
 // -> 2 = asynchronous-way:
 // Synchronous:It means that the code will be executed in a sequence, one after the other:It is blocking code, which means that the code will be executed in a sequential manner:
 // Asynchronous:It means that the code will be executed in a non-sequential manner;It is non-blocking code, which means that the code will be executed in a non-sequential manner:
