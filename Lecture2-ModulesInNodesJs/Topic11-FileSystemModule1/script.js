a20d6068c33979b9a21f15ce99296402b5522276
harish kumar <harishpathania150@gmail.com>
2025-01-11 10:59:00 +0530

    learn about fle system in nodejs

---
+++ Lecture2-ModulesInNodesJs/Topic11-FileSystemModule1/script.js
@@ -0,0 +1,9 @@
+console.log("here we are learning about file-system-module in node js");
+// (FS)-File System Module:It basically allows us to work with the file system on our computer:
+// V.IMP: We can use it to read, write, delete, update (data) in files and folders on our computer:It also help us to move our files and folders on the computer:
+// IMP: It is a core module in node js, so we don't need to install it separately;It is a synchronous and asynchronous module:
+
+
+
+// Synchronous:It means that the code will be executed in a sequence, one after the other;It is blocking code, which means that the code will be executed in a sequential manner:
+// Asynchronous:It means that the code will be executed in a non-sequential manner;It is non-blocking code, which means that the code will be executed in a non-sequential manner:
