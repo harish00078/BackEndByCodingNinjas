68ea830a466e3d5990228af1f1803f862fc8f9ee
harish kumar <harishpathania150@gmail.com>
2025-01-13 11:22:52 +0530

    learn about file system in nodejs

--- Lecture2-ModulesInNodesJs/Topic12-FileSystemModule2/script.js
+++ Lecture2-ModulesInNodesJs/Topic12-FileSystemModule2/script.js
@@ -1,1 +1,5 @@
+console.log("Learning About File-System Module");
+// (FS)-File System Module:It basically allows us to work with the file system on our computer or machine:
 
+// here we are learning about the (synchronous) methods of the file system module:
+// -> 2 = Creating a file:using the (writeFileSync) method:which is used to create a file and write data to it:it is a synchronous method:
