3050a28ddf6083952702cc729ce6db350d1c0579
harish kumar <harishpathania150@gmail.com>
2025-01-13 12:21:46 +0530

    learn about file system in nodejs

--- Lecture2-ModulesInNodesJs/Topic12-FileSystemModule2/script.js
+++ Lecture2-ModulesInNodesJs/Topic12-FileSystemModule2/script.js
@@ -15,15 +15,23 @@ const fs = require("fs");
 // with the help of the (writeFileSync) method:
 // V.IMP-NOTE:so some time when we write the data to the file:or try to create the new-file:then it will give us the error:because we may not have the permission to write to the file on user's computer:so that where to avoid that error we can use the (try-catch) block method:
  try{
-    const data = fs.writeFileSync("data.txt", "name:John Doe,age:25,city:New York");
+    const createFile = fs.writeFileSync("data.txt", "name:John Doe,age:25,city:New York");
  }catch(error){
     console.log(error,"Error Occurred");
  }
 
 
 // Reading the data from the file:with the help of the (readFileSync) method:
-const readData = fs.readFileSync("data.txt", "utf-8");
-console.log(readData);
+const readFile1 = fs.readFileSync("data.txt", "utf-8");
+console.log(readFile1);
 
 
 // -> 3 = Updating-file or updating-data in the file:
+// using the (appendFileSync) method:which is used to (update) the file or (add) data to the file:it is a synchronous method:
+// IMP:It takes 2 arguments/parameters:
+// -> 1 =  the (path) of the file:if it exits then it will update the file or its data:if doesn't exist then it will create a new-file: 
+// -> 2 =  the (data) that we want to write to the file:
+const updateFile = fs.appendFileSync("data.txt","\nName:david Doe,age:30,city:Los Angeles");
+
+const readFile2 = fs.readFileSync("data.txt", "utf-8");
+console.log(readFile2);
