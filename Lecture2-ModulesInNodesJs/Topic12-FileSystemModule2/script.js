8250c6e01a5459f4ca2a0e4515fa165bbc8b8dbd
harish kumar <harishpathania150@gmail.com>
2025-01-13 11:19:26 +0530

    learn about file system in nodejs

--- Lecture2-ModulesInNodesJs/Topic12-FileSystemModule2/script.js
+++ Lecture2-ModulesInNodesJs/Topic12-FileSystemModule2/script.js
@@ -1,4 +1,4 @@
-console.log("here we are learning about file-system-module in node js");
+
 // (FS)-File System Module:It basically allows us to work with the file system on our computer or machine:
 
 // here we are learning about the (synchronous) methods of the file system module:
