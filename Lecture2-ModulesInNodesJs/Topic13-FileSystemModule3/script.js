7d8fb8a59b74cee5363679778b2ddeb65cd5d8b1
harish kumar <harishpathania150@gmail.com>
2025-01-15 18:45:35 +0530

    learn about file system in nodejs

--- Lecture2-ModulesInNodesJs/Topic13-FileSystemModule3/script.js
+++ Lecture2-ModulesInNodesJs/Topic13-FileSystemModule3/script.js
@@ -1,24 +1,27 @@
 console.log("learning file system module in node js");
-// Here we are going to learn about (Asynchronous) file-system-module methods in node js:
+// Here we are going to learn about (Asynchronous-Methods) of file-system-module in node js:
 
 // V.IMP:the (Asynchronous) methods basically did not have the (async) keyword at the last of the method-name:the way we have with in the synchronous-methods:and we did not have do that because the (asynchronous) methods are (default) in the node js:and we can use them without using the (async) keyword at the last of the method-name:
 
-// we gonna learn about Asynchronous fS-module methods by performing CRUD-operations on a file:
+// we gonna learn about Asynchronous-methods of fS-module by performing CRUD-operations on a files same as we have learned with in the (synchronous-methods) of fs-module:
 
 // -> first import module:
 const fs = require("fs");
 
-// V.IMP-NOTE(working):In (Asynchronous) methods basically have the (callback) function as an (second-argument):as we have learn with in the (Asynchronous) working-system in js:through which we are gonna perform the (CRUD-operations) on the files or files-data:through which we are gonna perform the (CRUD-operations) on the files or files-data in asynchronous-way:
-// this difference between (synchronous) and (asynchronous) method:In (synchronous) methods we basically provide the (data) as an (argument/parameter) directly:
-// V.IMP: In (Asynchronous) methods, we don't provide the data as an argument directly to fs-methods as we have learned that to handle the (asynchronous-operations) we have to the the other methods or ways which basically help us to handle the (asynchronous-operations) or methods:
-// -> they take the (callback-function) or other-method and  ways which basically help us to handle the (asynchronous-operations):
-// V.IMP-NOTE:here we are using the asynchronous methods:so to handle the (asynchronous-methods):we have to provide them the (callback-function) as there last-argument:as we have learned that in asynchronous-system in js:
-// V.IMP:they take a callback function as their last argument, which is called when the operation is completed. The callback function has (two-arguments):
+// V.IMP-NOTE(working):In (Asynchronous) methods:we basically have the (callback) function as an (second or third-argument):as we have learn with in the working-system of (Asynchronous) in js:
+// -> 1 =  this difference between (synchronous) and (asynchronous) method:In (synchronous) methods we basically provide the (data) as an (argument/parameter) directly:
+// -> 2 =In asynchronous methods, data is not provided directly as an argument to file system (fs) methods. Instead, to handle asynchronous operations, other methods or mechanisms are used to manage these processes effectively:these methods basically help us to achieve the (asynchronous) working of js:
+// -> those other methods or mechanisms basically are (callback-function) or other-methods and ways which basically help us to handle or perform the (asynchronous-operations):
+
+
+// V.IMP-NOTE:here we are using the asynchronous-methods:so to handle the (asynchronous-methods):we have to provide them the (callback-function) as an (argument):as we have learned that in asynchronous-system working in js:
+// V.IMP:these methods mostly takes a callback function as their last- arguments:which is called when the operation is completed. The callback function has (two-arguments):
 // -> 1 = the first argument is an (error-object):
-// -> 2 = the second argument is the result of the operation:
-// IMP-NOTE: If an error occurred, the first argument will contain an error object, otherwise, the first argument will be null. The second argument will contain the result of the operation. 
-// V.IMP-NOTE: As we have learned with in the (Asynchronous)  or (event-loop) architecture in js: The (Asynchronous) methods are non-blocking, which means that the main-thread will not be blocked, and it can perform other tasks while the file operation is being performed. The (Asynchronous) methods are always preferred over the blocking methods:
-// and we have learned that (asynchronous) working of (js):Can be achieved with the help of (callbacks) or other-method of  js:
+// -> 2 = the second argument is the (result) of the operation:
+// IMP-NOTE: If an error occurred, the first argument will contain an error object, otherwise, the first argument will be null:The second argument will contain the result of the operation:
+
+
+// V.IMP-NOTE: As we have learned with in the (Asynchronous)  or (event-loop) architecture in js: The (Asynchronous) methods are non-blocking, which means that the main-thread will not be blocked, and it can perform other tasks while the file operation is being performed. The (Asynchronous) methods are always preferred over the blocking methods:and as we have learned that (asynchronous) working of (js):Can be achieved with the help of (callbacks) or other-methods and mechanisms which help us to handle the (asynchronous-operations) of js:
 
 
 
