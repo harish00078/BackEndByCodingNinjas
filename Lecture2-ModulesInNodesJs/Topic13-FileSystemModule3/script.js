eee67ec34bbb1db69401cdd08417eb13e5028272
harish kumar <harishpathania150@gmail.com>
2025-01-15 11:57:29 +0530

    learn about file system in nodejs

--- Lecture2-ModulesInNodesJs/Topic13-FileSystemModule3/script.js
+++ Lecture2-ModulesInNodesJs/Topic13-FileSystemModule3/script.js
@@ -1,1 +1,6 @@
 console.log("learning file system module in node js");
+// Here we are going to learn about (Asynchronous) file-system-module methods in node js:
+// IMP-NOTE:We learned about performing CRUD operations using blocking code with the FileSystem module, which means while file operations are being performed, our main-thread is blocked, and it can't perform any other task. Blocking code can make your app less responsive and slow. To avoid this, we can use non-blocking or asynchronous-methods available in the FileSystem module:
+
+// we gonna learn about Asynchronous fS-module methods by performing CRUD-operations on a file:
+
