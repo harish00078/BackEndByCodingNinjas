5ffe4f21c934b73280f107333040ef593eb05cff
harish kumar <harishpathania150@gmail.com>
2025-01-20 11:31:50 +0530

    learn about path modules

--- Lecture2-ModulesInNodesJs/Topic14-PathModules/script.js
+++ Lecture2-ModulesInNodesJs/Topic14-PathModules/script.js
@@ -24,9 +24,12 @@ const readingFile = fs.readFile('./src/database/data.txt','utf-8',(err,data)=>{
 
 // -> second:we gonna check about that how we can define the (path) to the file-system with the help of (path-module):
 // Steps To Follow:
-// IMP-1: so for defining-path to the (fs) with the help of (path-module):first we need to define the path separately:because we can't directly define the path to the (fs) with the help of (path-module) and path-module does not work that way:
-// with in the path-module:we have to define the path separately:we gonna define the path separately with in the const-variable:and with in that variable we are gonna define the path through the help of (methods) of (path-module):and then we can use that (path-variable) to define the path into the file-system:
-const filePath:
+// IMP-1: so for defining-path to the (fs) with the help of (path-module):first we need to define the path separately:because we can't directly define the path to the (fs) with the help of (path-module) and path-module does not work that way:because for creating path using (path-module) we need to use (methods) of (path-module):
+// that's why we have to define the path separately:we gonna define the path separately with in the const-variable:and with in that variable we are gonna define the path through the help of (methods) of (path-module):and then we can use that (path-variable) to define the path into the file-system:
+
+// IMP-2:these method of path-module:basically define the (path) in such-way that they can be easily understand by the any operating-system or machine's file-system:
+// -> so for creating path using (path-module):we have its (join) method:which is used to join the (path) of the file-system:
+// what does that (join) the path of file-system:means
 
 const readFileData = fs.readFile(,'utf-8',function(err,data){
     if(err){
