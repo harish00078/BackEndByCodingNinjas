81a0863a8788034cf0ac80600c6e92c33c00fcaa
harish kumar <harishpathania150@gmail.com>
2025-01-20 11:02:53 +0530

    learn about path modules

--- Lecture2-ModulesInNodesJs/Topic14-PathModules/script.js
+++ Lecture2-ModulesInNodesJs/Topic14-PathModules/script.js
@@ -2,5 +2,15 @@ console.log("learning about path module");
 
 // (path) module:It is used to work with the file-system on our computer or machine:
 // V.IMP: It is a core module in node js, so we don't need to install it separately:
-// V.IMP-NOTE:path-module basically help us to define the paths for the file-system:In such way that our file-system can easily understand the path of the file which we are trying to access:With in the any form of operating-system or machine:because every operating-system or machine has its own way of defining the path for the file-system:
-// V.IMP-NOTE:because of this reason, we need to use the path module in order to define the path for the file-system:other wise our file-system will not gonna be able to work on the other operating-system or machine:and that's where our application will crash:that is why we need to use the path module in order to define the path for the file-system:
+// V.IMP-NOTE:path-module basically help us to define the paths for the file-system:In such way that our file-system can easily understand the path of the file which we are trying to access:With in the any form of operating-system or machine:because every operating-system or machine has its own way of defining the path for the file-system:that why we need to use path-module to define the paths for the file-system:so that our application can easily understand the path of the file which we are trying to access and did not get crashed:
+
+
+// here we learn about the path module:
+// -> Import (fs-module):because it works with it:
+const fs = require("fs");
+// -> Import (path-module):we gonna learn to use it with the (fs-module):
+const path = require("path");
+
+// we can understand with the help of example:like we want to read some file from the another folder:
+// -> first:we check the simple-way of doing it:
+
