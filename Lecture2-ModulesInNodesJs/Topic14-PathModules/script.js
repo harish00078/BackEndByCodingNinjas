f7e3d94559e9094db684a64441b8329b2560a6ff
harish kumar <harishpathania150@gmail.com>
2025-01-20 10:56:42 +0530

    learn about path modules

--- Lecture2-ModulesInNodesJs/Topic14-PathModules/script.js
+++ Lecture2-ModulesInNodesJs/Topic14-PathModules/script.js
@@ -2,5 +2,5 @@ console.log("learning about path module");
 
 // (path) module:It is used to work with the file-system on our computer or machine:
 // V.IMP: It is a core module in node js, so we don't need to install it separately:
-// V.IMP-NOTE:path-module basically help us to define the paths for the file-system:In such way that our file-system can easily understand the path of the file which we are trying to access:
-// V.IMP-NOTE:With in the any form of operating-system or machine:because every operating-system or machine has its own way of defining the path for the file-system:
+// V.IMP-NOTE:path-module basically help us to define the paths for the file-system:In such way that our file-system can easily understand the path of the file which we are trying to access:With in the any form of operating-system or machine:because every operating-system or machine has its own way of defining the path for the file-system:
+// V.IMP-NOTE:because of this reason, we need to use the path module in order to define the path for the file-system:other wise our file-system will not gonna be able to work on the other operating-system or machine:and that's where our application will crash:that is why we need to use the path module in order to define the path for the file-system:
