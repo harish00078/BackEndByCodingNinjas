094dec1f968ace7123c09b42ac9c16fb44865dc7
harish kumar <harishpathania150@gmail.com>
2025-01-20 17:13:46 +0530

    learn about path modules

--- Lecture2-ModulesInNodesJs/Topic14-PathModules/script.js
+++ Lecture2-ModulesInNodesJs/Topic14-PathModules/script.js
@@ -93,4 +93,4 @@ console.log('directory name',directoryName);
 // => 5[V.IMP-Topic] = (__dirname) is a global-variable it is not a method: In Node.js it returns the path of the current-working-directory:
 // V.V.IMP-NOTE:(__dirname) is a global variable that returns the directory-name of the current-module or file:where we are using it:
 // It provides the absolute-path of the directory which containing the one or more script-files and modules we can say:
-// V.IMP-NOTE:which means with the help of it:we can easily access the all other files:while working on a one-particular file:we did not have to define the (file-path) or anything:we can simply use it and through that we gonna be able to access the directory-path or folder of this (files): and know from where we can easily access all other files:
+// V.IMP-NOTE:which means with the help of it:we can easily access the all other files:while working on a one-particular file:we did not have to define the (file-path) or anything:we can simply use it and through that we gonna be able to access the directory-path or folder of this (files): and know from where we can easily access all other-files without defining the (file-path):
