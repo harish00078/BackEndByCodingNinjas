8d35548891735946098a2bc9b235d280eab7f651
harish kumar <harishpathania150@gmail.com>
2025-01-23 11:07:15 +0530

    learn about more modules

--- Lecture3-ExploringMoreModules/Lecture1-DebuggingNodejs/script.js
+++ Lecture3-ExploringMoreModules/Lecture1-DebuggingNodejs/script.js
@@ -2,14 +2,14 @@
 function calculateTotal(products) {
   let total = 0;
   products.forEach((product) => {
-    total += product.quality * product.quantity;
+    total += product.price * product.quantity;
   });
   return total;
 }
 const productsList = [
-  { name: "laptop", quality: 50, quantity: 2 },
-  { name: "mobile", quality: 25, quantity: 1 },
-  { name: "charger", quality: 30, quantity: 2 },
+  { name: "laptop", price: 50, quantity: 2 },
+  { name: "mobile", price: 25, quantity: 1 },
+  { name: "charger", price: 30, quantity: 2 },
 ];
 // expected output: 185
 const grandTotal = calculateTotal(productsList);
