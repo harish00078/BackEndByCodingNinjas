b903ecc47faa799b86086275e9904f5cd8516bb1
harish kumar <harishpathania150@gmail.com>
2025-01-25 10:35:58 +0530

    learn about using redux with vanilla js

--- Lecture3-ExploringMoreModules/Lecture2-DebuggingInVsCode/script.js
+++ Lecture3-ExploringMoreModules/Lecture2-DebuggingInVsCode/script.js
@@ -11,6 +11,13 @@ function calculateTotal(products) {
     { name: "mobile", price: 25, quantity: 1 },
     { name: "charger", price: 30, quantity: 2 },
   ];
+
+  // this function gonna print the total of all the products:\
+  function printTotalValue(value){
+    console.log('Total:',value);
+  }
+
   // expected output: 185
   const grandTotal = calculateTotal(productsList);
-  console.log('grand Total:',grandTotal);
+ // here we gonna use the printTotalValue function to print the total value of the products:which we have calculated above:with in the grandTotal variable:
+ printTotalValue(grandTotal);
