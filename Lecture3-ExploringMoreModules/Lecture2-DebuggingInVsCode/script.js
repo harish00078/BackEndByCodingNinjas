3a69e66ce46d5c0d3f00cff11a8fe456abe5d331
harish kumar <harishpathania150@gmail.com>
2025-01-25 10:29:07 +0530

    learn about using redux with vanilla js

--- Lecture3-ExploringMoreModules/Lecture2-DebuggingInVsCode/script.js
+++ Lecture3-ExploringMoreModules/Lecture2-DebuggingInVsCode/script.js
@@ -1,1 +1,16 @@
-console.log('learning debugging in vscode');
+// For that here we have same code example:through which we gonna understand that hoe we can debug the code in node js:
+function calculateTotal(products) {
+    let total = 0;
+    products.forEach((product) => {
+      total += product.price * product.quantity;
+    });
+    return total;
+  }
+  const productsList = [
+    { name: "laptop", price: 50, quantity: 2 },
+    { name: "mobile", price: 25, quantity: 1 },
+    { name: "charger", price: 30, quantity: 2 },
+  ];
+  // expected output: 185
+  const grandTotal = calculateTotal(productsList);
+  console.log('grand Total:',grandTotal);
