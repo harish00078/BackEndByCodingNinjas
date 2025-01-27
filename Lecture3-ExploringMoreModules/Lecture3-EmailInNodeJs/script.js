68b73dde60b0f8aa6b696c6811f754865f8a49b9
harish kumar <harishpathania150@gmail.com>
2025-01-27 10:54:08 +0530

    learning about emailing in nodejs

--- Lecture3-ExploringMoreModules/Lecture3-EmailInNodeJs/script.js
+++ Lecture3-ExploringMoreModules/Lecture3-EmailInNodeJs/script.js
@@ -5,12 +5,17 @@ const nodemailer = require('nodemailer');
 // IMP: here we are gonna be configure our (nodemailer) to send an emails:its gonna be the async-function:so for converting a function to async-function we just need to add the async-keyword in front of the function-keyword:
 // IMP: we are gonna be using the async-await syntax to send an email:
 
-// how we gonna configure the nodemailer to send-emails:
-// 1: we need to create a transporter:which is gonna be the object of the nodemailer:  
 
+// => how we configure the nodemailer to send-emails:
+// 1: first we have to configure the (email):through which we are basically gonna be sending the emails to the (user):this (email) basically first connect to the (email-server):and then send the email to the (user):
+// -> for doing that:we need to create a (transporter):which is basically gonna help us to send the email to the (email-server):and then the (email-server) is gonna be sending the email to the (user):
 
 async function sendEmail(){
 
+    // 1:To create a (transporter):we need to call the (createTransport)-method:which is available in the (nodemailer-module):
+    // -> this (createTransport)-method is gonna be taking an object:which is gonna be containing the (service):which we are gonna be using to send the email:
+    // -> this (service) basically gonna be the (email-server):which we are gonna be using to send the emails to the (user):
+    // -> we gonna be using the (gmail) service to send the emails to the (user):
 
 
 }
