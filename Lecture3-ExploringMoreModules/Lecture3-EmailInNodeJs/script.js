a1ae0f9a1b9fff926f30bb648feb328a45ec9234
harish kumar <harishpathania150@gmail.com>
2025-01-27 11:59:25 +0530

    learning about emailing in nodejs

--- Lecture3-ExploringMoreModules/Lecture3-EmailInNodeJs/script.js
+++ Lecture3-ExploringMoreModules/Lecture3-EmailInNodeJs/script.js
@@ -14,14 +14,13 @@ async function sendEmail(){
 
     // 1:To create a (transporter):we need to call the (createTransport)-method:which is available in the (nodemailer-module):
     // -> this (createTransport)-method is gonna be taking an object:which is gonna be containing the configuration of (email) and its (email-service) server:which we used to send the emails to the (users):
-    // V.IMP: (createTransport)-method basically takes the (object) as an argument where we have basically configured the (email) and its (email-service) server:and after that it is going to be returning the (transporter-object) to us:
+    // V.IMP: (createTransport)-method basically takes the (object) as an argument where we have basically configured the (email) and its (email-service) provider or the server:and after that it is going to be returning the (transporter-object) to us:
 
     // V.IMP-NOTE:With in the (createTransport)-method object: we basically have to provide or configure multiple things:
-    // -> 1 = the (host) of the (email-service) server:
-    // -> 2 = the (port) of the (email-service) server:
-    // -> 3 = the (user) and (password) key:
+    // -> 1 = the (host) of the (email-service) server:like (Gmail):
+    // -> 2 = the (user) and (password) key:To authenticate the user:who is sending the email:
     // and many more things:which depend on the (email-service) provider or server and our requirements:
-    
+
     const transporter = nodemailer.createTransport({
 
         // => 2:know there another thing:through which basically we connects to the (servers) and those are the protocols:the same user does when they connect with our server:through the (http) or (https) protocol:
