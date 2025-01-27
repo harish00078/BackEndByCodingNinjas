0f6918b1f519ae1cab5a50b8e60179dcf3cf4e09
harish kumar <harishpathania150@gmail.com>
2025-01-27 10:58:40 +0530

    learning about emailing in nodejs

--- Lecture3-ExploringMoreModules/Lecture3-EmailInNodeJs/script.js
+++ Lecture3-ExploringMoreModules/Lecture3-EmailInNodeJs/script.js
@@ -1,7 +1,7 @@
 // 1: Import nodemailer-module:
 const nodemailer = require('nodemailer');
 
-// 2: configure the email and send it:
+// 2: configure the emailer-service to send emails:
 // IMP: here we are gonna be configure our (nodemailer) to send an emails:its gonna be the async-function:so for converting a function to async-function we just need to add the async-keyword in front of the function-keyword:
 // IMP: we are gonna be using the async-await syntax to send an email:
 
