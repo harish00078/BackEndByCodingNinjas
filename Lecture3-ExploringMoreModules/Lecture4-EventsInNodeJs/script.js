0931879b8da06816a2541da6ae02bd5a9d4c8499
harish kumar <harishpathania150@gmail.com>
2025-01-31 11:58:07 +0530

    learning about events in nodejs

--- Lecture3-ExploringMoreModules/Lecture4-EventsInNodeJs/script.js
+++ Lecture3-ExploringMoreModules/Lecture4-EventsInNodeJs/script.js
@@ -3,7 +3,11 @@ console.log("Learn about events in Node.js");
 // V.IMP NOTE 1:there are multiple-architectures and ways to work with nodejs:using which we basically handle things in nodejs:
 // like:(any-action),(http-request),(file-read-write),(file-upload-download),(database-operations),(etc):
 
-// ->1 = Event-Driven Architecture (Most Popular):
+// ->1 = Event-Driven Architecture (Most Popular):because everything in nodejs is based on events:because we have to perform actions everywhere on nodejs to work with it:and we handle those actions or things in nodejs with the help of events:
+// -> that's why we use or call nodejs as event-driven architecture:
+// IMP:some of the most common events in nodejs are:
+// -> listening for http-connections using (listen) method is also the event:
+// -> handling http-request and response those are also events and etc:
 
 // One of the most widely used architectures in Node.js is the event-driven architecture.
 // In this approach, we have events and listeners that listen for those events and trigger specific actions when they occur.
@@ -21,5 +25,20 @@ console.log("Learn about events in Node.js");
 
 // ************************************************************/
 // V.IMP-NOTE2: In this architecture we can create own events and listen to them in nodejs:
-// -> In this architecture we have (EventEmitter) class which is basically used to emit events and then we can listen to those events and perform some action:
+// -> In this architecture we have (EventEmitter) class which is basically used to emit-events and then we can listen to those events and perform some action:
 // -> (EventEmitter) class is a core module in nodejs:so we don't need to install it separately:we can directly use it by importing it.
+
+// ************************************************************/
+// => First: Here we gonna be learn about that we can handle:(http-requests) in nodejs using events or event-driven architecture:
+// -> for that in nodejs:we have build-in (events) same as we have in browser-javascript:
+// -> but they are different acc to their working or different environment:because in nodejs we have to handle (http-requests) using events:
+// V.IMP:basically the use of events or we can say how we can define which event we have to use for the particular (http-request) in nodejs:
+// -> it depends on the (http-method) which are getting with in the request:
+
+// most-common nodejs built-in events are:
+// -> 1 = request: this event is used to handle (http-requests) in nodejs:
+// -> 2 = response: this event is used to handle (http-responses) in nodejs:            
+// -> 8 = data: this event is used to handle (http-data) in nodejs:
+
+
+
