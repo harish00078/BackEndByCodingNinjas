64c6a3ae144702f5971596821562e2daeba85777
harish kumar <harishpathania150@gmail.com>
2025-02-21 17:38:50 +0530

    learning about Response-object and its end method

--- Lecture3-ExploringMoreModules/Lecture6-Res-End-Method/script.js
+++ Lecture3-ExploringMoreModules/Lecture6-Res-End-Method/script.js
@@ -14,10 +14,17 @@ const server = http.createServer((req, res) => {
   // V.IMP NOTE: When using the .end() method within any statement or block that has its own scope, we must also include the return statement to prevent further execution of the code or request.
   // -> This ensures that our system does not encounter the ERR_HTTP_HEADERS_SENT error:
   // -> which occurs when a request has already been ended but where we are handling it that statement or block does not stopped:because of that (function) which executes the request or statement and block is not terminated or stop.and that why its gonna jump on another endpoint or method unintentionally.for doing the further execution of the code:
- 
+
   if (req.url == "/first") {
     return res.end("this is first response");
   }
+  // else {
+  // default response to default-request:
+  //   res.end("Hello World:Its default response");
+  // }
+
+  // IMP-NOTE:If we are handling the specific request with the help of if-statement:then we can use the (else-statement) to handle the default request:instead of using the (return) method with the (end) method of (response-object) in the (if) statement:
+  // use of return-method basically depend on that how we are handling the (requests):
 
   // default response to default-request:
   res.end("Hello World:Its default response");
