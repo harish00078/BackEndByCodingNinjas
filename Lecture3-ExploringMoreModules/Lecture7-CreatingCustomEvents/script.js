add3cbf6f8d98dd832794fab4a04a3e264285eae
harish kumar <harishpathania150@gmail.com>
2025-02-21 18:17:53 +0530

    learning about custom events or event-driven-architecture

--- Lecture3-ExploringMoreModules/Lecture7-CreatingCustomEvents/script.js
+++ Lecture3-ExploringMoreModules/Lecture7-CreatingCustomEvents/script.js
@@ -1,5 +1,10 @@
 console.log("Learning About Creating-Custom-Events Or Event-Driven-Architecture");
 
+// IMP-NOTE:Understand it through an example:
+// -> we are getting the (post) from the (user):and we wanna handle through the (events) or (event-driven-architecture):
+// -> so,we can create the (custom-events) for that:and through that (custom-events) we can handle the (post) from the (user):
+// -> through that (custom-event) we can do the (multiple-operations) on that (post) by simply creating the multiple (event-listeners) for that:
+// V.IMP:It help us to handle the (post) from the (user) in a (modular-way) or in a (asynchronous-way):that what event-driven-architecture is all about or help us do:To handle things in a modular-way or in a asynchronous-way:
 
-// IMP-NOTE:for example:we are getting the (post) from the (user):and we wanna handle through the (events) or (event-driven-architecture):
-// so,we can create the (custom-events) for that:and through that (custom-events) we can handle the (post) from the (user) and we can do the (operations) on that (post) like (save,delete,update) etc.
+
+// 1. Creating the Custom-Event:
