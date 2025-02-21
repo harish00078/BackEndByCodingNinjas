ff97bb0536516944fa81768e80a5fa7bbc38c315
harish kumar <harishpathania150@gmail.com>
2025-02-21 18:24:37 +0530

    learning about custom events or event-driven-architecture

--- Lecture3-ExploringMoreModules/Lecture7-CreatingCustomEvents/script.js
+++ Lecture3-ExploringMoreModules/Lecture7-CreatingCustomEvents/script.js
@@ -7,5 +7,11 @@ console.log("Learning About Creating-Custom-Events Or Event-Driven-Architecture"
 // V.IMP:It help us to handle the (post) from the (user) in a (modular-way) or in a (asynchronous-way):that what event-driven-architecture is all about or help us do:To handle things in a modular-way or in a asynchronous-way:
 
 
-// Implementing the Event-Driven-Architecture:
-// IMP-NOTE:Acc to example we are getting the post the (user):but here we did not have any interaction-point with the user:
+// => Implementing the (custom-event) or Event-Driven-Architecture:
+
+// IMP-NOTE-1:Acc to example we are getting the post the (user):but here we did not have any interaction-point with the user:
+// so for representing the (user):we can create the (user) Class:and through that we can pass the (post) as it is from the (user):
+// IMP:we gonna do that by using (User-class):we can create the (user-object):and with in that (user-object) we can pass the (post):
+class User{
+
+}
