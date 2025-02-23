ee7f499e11cafaab940c3ec61317d0f71b298750
harish kumar <harishpathania150@gmail.com>
2025-02-23 17:46:48 +0530

    learning about custom events or event-driven-architecture

--- Lecture3-ExploringMoreModules/Lecture7-CreatingCustomEvents/eventListeners.js
+++ Lecture3-ExploringMoreModules/Lecture7-CreatingCustomEvents/eventListeners.js
@@ -1,6 +1,6 @@
-// IMP:we can use this file
+// IMP:Here we are importing the (User) class from the (clientEvents.js) file: through which we are gonna be able to create the (post) from the (user) and emit it as an event:
 
-import { UserEvents } from "./clientEvents";
+import { UserEvents } from "./userEvents";
 
 const userEvent = new UserEvents();
 
