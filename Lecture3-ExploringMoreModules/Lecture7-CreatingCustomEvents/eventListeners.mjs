c916b007266e7499a2445178617564e8fe61bb6d
harish kumar <harishpathania150@gmail.com>
2025-02-27 11:00:52 +0530

    learning about custom events or event-driven-architecture

--- Lecture3-ExploringMoreModules/Lecture7-CreatingCustomEvents/eventListeners.mjs
+++ Lecture3-ExploringMoreModules/Lecture7-CreatingCustomEvents/eventListeners.mjs
@@ -6,7 +6,8 @@ import { UserEvents } from "./userEvents.mjs";
 // This instance or object will be responsible for emitting the post as an event.  
 
 // V.IMP-NOTE: We utilize the properties of the UserEvents-class to emit the post as an event through that (instance) or (object):  
-// These properties are defined within the UserEvents-class, allowing us to trigger and handle events efficiently.
+// These properties are defined within the UserEvents-class, allowing us to trigger and handle events efficiently:
+
 const user = new UserEvents(); 
 
 // Function to simulate saving the post to a database.
@@ -29,11 +30,15 @@ function updateTimeLine(){
 
 // IMP: Here we are listening to the (postCreated) event and handling it through multiple event listeners.
 // Whenever the (postCreated) event is emitted, the registered event listeners (functions) will execute in order.
+// V.IMP:for listening to the (postCreated) event:we are using the (addListener) method of (nodejs):
 user.addListener("postCreated", saveToDatabase);
 user.addListener("postCreated", sendNotification);
 user.addListener("postCreated", updateTimeLine);
 
 
-// V.IMP-NOTE: Here we got the (post) from the (user):through the help of (request) method.
-// -> after getting the (post)
+// V.IMP-NOTE: Here we got the (post) from the (user):through the help of (http-request) method.
+// -> after getting the (post) from the (user):we have to emit the (post) as an event:
+// -> IMP: for that we have to use the (properties) of the (object):which we created from the (UserEvents-class):and where we have defined the (properties) to emit the (post) as an event.
+// -> IMP: Here we are using the (createPost) method to emit the (post) as an event.
+// -> IMP: This method will emit the (post) as an event and will trigger all the event listeners that are listening to the (postCreated) event.
 user.createPost('hey we have new post from the user');
