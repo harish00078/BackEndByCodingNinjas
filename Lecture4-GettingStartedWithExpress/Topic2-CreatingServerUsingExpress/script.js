ca5dc8ba9af76e67071a88a909bb675dac27aa13
harish kumar <harishpathania150@gmail.com>
2025-03-08 17:33:42 +0530

    learning about express

--- Lecture4-GettingStartedWithExpress/Topic2-CreatingServerUsingExpress/script.js
+++ Lecture4-GettingStartedWithExpress/Topic2-CreatingServerUsingExpress/script.js
@@ -3,3 +3,6 @@
 
 // ->1: Importing the express-module:
 const express = require('express');
+
+// ->2: Creating (server) using express:we simply have to call or invoke the (express) function or express-module:and store it in a variable:
+// -> and that variable will be our server-instance or server-object:through which we can access all the methods and properties of the server:to interact or work with the server:
