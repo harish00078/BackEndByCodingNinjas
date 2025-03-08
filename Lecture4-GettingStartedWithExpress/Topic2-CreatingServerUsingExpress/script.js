bd0c415622e7face078d72101e5e3535c36f9a67
harish kumar <harishpathania150@gmail.com>
2025-03-08 17:39:45 +0530

    learning about express

--- Lecture4-GettingStartedWithExpress/Topic2-CreatingServerUsingExpress/script.js
+++ Lecture4-GettingStartedWithExpress/Topic2-CreatingServerUsingExpress/script.js
@@ -7,3 +7,8 @@ const express = require('express');
 // ->2: Creating (server) using express:we simply have to call or invoke the (express) function or express-module:and store it in a variable:
 // -> and that variable will be our server-instance or server-object:through which we can access all the methods and properties of the server:to interact or work with the server:
 const server = express();
+
+
+
+// ->3:after creating server:we have to create (port-number) for the server:
+// -> so that the server can listen for the incoming requests which are coming on the particular port-number:
