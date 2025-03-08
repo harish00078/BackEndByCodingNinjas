e435fecebe2ae63e7d1e34ac485d94d3c98d2810
harish kumar <harishpathania150@gmail.com>
2025-03-08 16:58:35 +0530

    learning about express

---
+++ Lecture4-GettingStartedWithExpress/Topic2-CreatingServerUsingExpress/script.js
@@ -0,0 +1,5 @@
+// Here we are creating the server using (Express) framework:
+// We need to use the (express-module) to create the server with the help of the Express-framework:
+
+// ->1: Importing the express-module:
+const express = require('express');
