5eaabdfb8a1950cc7161f51d06d3d7232f8062d7
Apple <apple@Apples-MacBook-Pro.local>
2025-06-30 20:06:19 +0530

    removedsome comment error

--- Lecture1-GettingStartedWithNodeJs/Topic11-ReturningHtmlAsResponse/code/script.js
+++ Lecture1-GettingStartedWithNodeJs/Topic11-ReturningHtmlAsResponse/code/script.js
@@ -1,6 +1,6 @@
 // Learn to return HTML as a response:
 const http = require("http");
-// IMP = here we are importing the (fs) module or libraray:it is a built-in module in nodejs:which known as (file-system) module:using this module we can access or work with files of our system:
+// IMP = here we are importing the (fs) module or library:it is a built-in module in nodejs:which known as (file-system) module:using this module we can access or work with files of our system:
 const fs = require("fs");
 // IMP = here we are creating a server using the http module:we are using the (createServer) method of (http) module:
 const server = http.createServer((req, res) => {
