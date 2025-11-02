c3e2aba16f8ddc265304582ac1e565db15451280
harish kumar <harishpathania150@gmail.com>
2025-11-02 17:52:17 +0530

    revise about commonjs syntax

--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithmetic.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/arithmetic.js
@@ -1,142 +1,102 @@
-// here we gonna see that:how Modules are help full in Nodejs:
+// Understanding Modules in Node.js
 
-// Modules help us to do two and more things:
-// => first =  using modules we can share our code.or we can say that the particular section of our code-file from one-file to another-file:with the help of modules we can use something on the other files which we have created on the one particular file:
-// => Secondly = V.V.IMP(Note)= In Node.js, with the help of modules, we are able to connect our code or its multiple files:within one file and that one file is basically connected to our main app-file or server-file:We can also connect those files one by one with the main server-file. Instead of combining them first into one file:this is how we connect multiple files with in our Node.js or server application:same thing we do with in the front-end section library (react)  application as well:
-// => third = V.V.IMP(NOTE):modules syntax allows us to import built-in modules (like 'fs' for file system operations) and other libraries that we can install into our application using Node.js environment. This enables us to access and use them in our application files:
+// Modules in Node.js are essential for organizing, reusing, and sharing code. They allow us to:
+// 1. Export specific parts of our code from one file and import them into another, making our codebase more organized and maintainable.
 
-// IMP => we have two ways to do that:these are not types they are the ways that how we can have or create modules and access them after that:
-// V.V.IMP = we have two diff syntaxes to create modules in nodejs:
-// -> 1 = CommonJs Module syntax:
-// -> 2 = ES6 Module syntax:
+// 2. because of this we can (Split) our application into multiple files, which can be connected to the main app or server file. This modular-approach is similar to using components in React and makes code easier to manage.
+// 3. Import other-type and built-in modules (like 'fs' for file system operations) and third-party libraries through modules, enabling us to leverage a wide range of functionality in our applications.
 
-// ===> In this Topic-file we will specifically gonna be learn about (CommonJs Module syntax):
+// There are two main module systems in Node.js:
+// 1. CommonJS Module Syntax (default in Node.js)
+// 2. ES6 Module Syntax
 
-// => first = here we see that how we can do that with the help of (commonjs) modules syntax or method:
-// for that here we gonna create the two arithmetic functions:both of these arithmetic functions gonna get the same type of (argument) variables:
+// In this file, we'll focus on the CommonJS module system.
 
-// function sum(x, y) {
-//   return x + y;
+// Demonstrating CommonJS Module Syntax in Node.js:
+
+// In Node.js, modules help organize and reuse code. The CommonJS module system (the default in Node.js) uses `require` to import and `module.exports` (or `exports`) to export code between files.
+
+// => Know we have different-syntax-types available to export things using CommonJS-syntax:
+// -> but they mainly lies under two-categories:
+// -> 1 = Exporting one or multiple things as an Object: (properties-of-object):
+// -> 2 = Exporting a Single thing Directly: (direct or non-property export):
+
+// --- 1. Exporting one or multiple things as an Object: ---
+// => know with in this category mainly we have three-types of syntax available to export things:
+
+// (First type) — You can define your functions separately and export them as properties of an object.This approach is most useful when you want to export multiple related functions or values together,making it easy to organize and access all relevant features through a single imported object.For example, you might group arithmetic functions into one exported object for cleaner, modular access.
+// (NOTE) = with in first-type we have also have two-ways of syntax available to export things:
+// -> 1 = You can define your functions-separately and export them as properties of an object:
+// -> 2 = You can use the shorthand syntax to export things:(where we use the (same-name) for the (function) and the (property) of the (object)):
+
+// -> 1 = You can define your functions-separately and export them as properties of an object:
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// function div(a, b) {
+//   return a / b;
 // }
 
-// function div(x, y) {
-//   return x / y;
-// }
-
-// V.V.IMP: In commonjs modules method or syntax way we use (module.exports) keywords with (object) or without the (object) to (export) things:we use them according to how many of things we want to export:same thing we do with in the (ES6-module-syntax) as well:
-
-// V.V.IMP(NOTE):CommonJs basically uses a (object) to (export) things that's why it receives or have a (object) with in it:In which we assign our function,variables,or anything that we want to export from our file:
-
-// IMP(NOTE): And that's why its gonna be export them as (key-value) pairs or (Properties):same thing that we have with in the simple-objects:
-
-// => CommonJs Module:CommonJS is the (default) module-system in Node.js which uses the (require) function to import-modules and the (module.exports) object to export them:
-// V.IMP-NOTE = here these functions gonna be exports as properties because of the (object) usage:and we have also already know that if we export (functions) as properties then those (functions) become's (methods):
-
-// V.IMP(NOTE)=> we can also use (module.exports) or (commonjs-export) syntax in (three) and more ways:but the most common once are these three ways:
-// -> 1 => use (module.exports) keywords with the help of (objects):
-// -> 2 => use (module.exports) keywords without the help of (objects):
-// -> 3 => use only (exports) keyword with the help of (objects) and without (objects):
-
-// => [first-way]:Is with the help of (objects):with in this first-way:we also have the multiple ways do it as well:mainly its in two-ways:
-
-// -> [1-first] == is that we can do it as key-value pairs by providing (keys) to our functions:with in the module.exports object:
+// (NOTE) = here we have exported the (sum) and (div) functions as properties of an object:and we have also assigned the (sum) and (div) functions to the (addition) and (divide) properties of the (module.exports) object:
 
 // module.exports = {
-//   Add: sum,
-//   Div: div,
+//   addition: sum,
+//   divide: div,
 // };
 
-// -> [2-second] = Is that we can simply provide our created-functions and other things as it is they are as keys or values we can say with in the module.exports object:and they will automatically becomes the (key-value) pairs by there self:
+// -> 2 = You can use the shorthand syntax to export things:
+// module.exports = { sum, div };
 
-// module.exports = {
-//   sum,
-//   div,
-// };
+// -> (Second type): Instead of defining functions separately and then attaching them to `module.exports`, you can define and export them together in one step:mostly use of it that we want to create want to create and exports things separately instead of putting them together in one object:
+// (Explanation): This means you create and export your functions at the same time by directly assigning them as properties on the `module.exports` object:
+// (Note): Here, you add properties (like `.sum` or `.div`) directly to `module.exports` using dot notation, allowing you to define and export individual functions in a concise way.
 
-// =>[second-way]:In the second-way:we do it without the (objects):we also have the multiple ways here to do it as well:
-//V.V.IMP(NOTE) = here we gonna see that how we can use the (commonjs-modules) export syntax or method directly with the functions and others things as well:
-// V.IMP = here we are using it with the functions:And we also gonna see that how we can use it with both the function-types:
-// (simple-functions) and (arrow-functions):
+module.exports.sum = (a, b) => {
+  return a + b;
+};
+module.exports.div = function (a, b) {
+  return a / b;
+};
 
-// [first-type]: Simple-Functions:means those functions which has been created using the (function) keyword:
-// here we are using the Commonjs Export-Syntax with (simple-function) directly without even declaring the (object-first) with in it:
+// --- 2. Exporting a Single Function Directly ---
+// If you only want to export one function, you can assign it directly to `module.exports`:
 
-// V.V.IMP = we also have two ways to do it:
-
-// -> [first-way] => In first-way we create the function in the (simpler-way) using (function) keyword:and then simply pass that function to the (module.exports) keyword using the (equalTo) sign:
-
-// V.V.IMP-[NOTE]:-> this is the only way we can export our own created function as (function) on nodejs with its (CommonJs) syntax:
-
-module.exports  = function sum(x, y) {
+module.exports = function sum(x, y) {
   return x + y;
-}
+};
 
-// -> [Second-way] => In this way we gonna create the function in different way:by providing our (function-name) directly to the (module.exports) keywords with the help of (.) dot-keyword:and then create the function using (function) keyword:and after that assign it through (equalTo) sign to the (module.exports) keywords with its (function-name):
-
-// V.V.IMP-[NOTE]:we have use this way in our (backend-codeial) project:we can also use the (async-await) method with it:mostly we are gonna be use this way in our (backend) projects:
-// V.V.IMP-[NOTE]:-> we can also use this same-way:with the (arrow-functions) as well:
+// --- 3. Exporting Multiple Functions as Properties ---
+// You can attach functions directly as properties to `module.exports`:
 
 // module.exports.sum = function (x, y) {
 //   return x + y;
-// }
-// module.exports.multiple = function (x, y) {
+// };
+// module.exports.multiply = function (x, y) {
 //   return x * y;
-// }
-
-// [Second-type]: Arrow-Functions:means those function which we are not created using the (function) keywords:they are created using the (arrow-functions) method:
-// IMP = here we gonna use the (arrow-functions) directly with the module.exports keyword by declaring its function-name with them as well:same thing we did with in the simple-function's second-way:
-// And we can declare (arrow-functions) method by combining (equalTo(=)) and (greaterThan(>)) sign:
-
-// V.IMP-NOTE = here these functions is also gonna be exported as properties:
-// V.V.IMP => we can also use (arrow-functions) directly with the (module.exports) keywords in two-ways:same as the simple-functions:
-
-// [first-way]: Is that we can create the simple (arrow-function):
-// IMP[NOTE] = but it is not the compatible way that's why we did not use it.because in this syntax we are gonna be able to (name) our (arrow-function):
-// module.exports = (x,y) =>{
-//     return x + y;
-// }
-
-// [second-way]: Is that we can create the (arrow-function) with the help of (module.exports) keywords: by declaring its function-name with them as well: and then assign it to the (module.exports) keywords with the help of (equalTo) sign: same way we did with the (simple-functions):
-
-// module.exports.multiple = (x, y) => {
-//     return x * y;
 // };
 
-// -> [Third-way] => we can also do it only with the help of (exports) keyword:we did not have to use the (module) keyword with it:
-// V.IMP-NOTE: The Exports method basically uses the (module.exports) keywords internally:but its different then (module.exports) method:because the(exports) method does not able to export the (classes) and (functions): It can only be able to export the (variables) and (objects) and etc:
-// V.V.IMP-[NOTE] => This method (specifically) export the functions or anything as properties:
+// --- 4. Using Arrow Functions ---
+// Arrow functions can also be exported in the same way:
 
-// V.V.IMP = here we gonna see that: how we can use this particular method with both ways (objects) and (non-objects):
+// module.exports.multiply = (x, y) => x * y;
 
-// ->[first-way] => using-Object:we can not use this way with the objects:
-//  exports = {
-//      sum,
-//  }
+// --- 5. Using the `exports` Shortcut ---
+// `exports` is a shorthand for `module.exports` and is commonly used to attach properties:
 
-// V.IMP = here we gonna use it with both the function-types:but without the objects declaration:
-// (simple-functions) and (arrow-functions):
-
-// => [simple-function]:
-// V.V.IMP-NOTE =  here it also gonna be export the simple-functions as properties:not as (function) which we have with the (module.exports) keywords:
-
-exports.subtraction = function(x, y) {
+exports.subtraction = function (x, y) {
   return x - y;
-}
+};
 
-// -> arrow function: here this arrow-function is also gonna be export as properties:
+// You can also use arrow functions:
 
-// exports.sum=(x, y)=> {
-//   return x + y;
-// }
+// exports.sum = (x, y) => x + y;
 
-// V.V.IMP[NOTE]:there one more-way to use (exports) keyword:and that we can use if we have already created the thing:which we want to be export:then we can use this way:
-// we can use this way: if we are not using the (module.exports) keywords:then we can use the (exports) keyword separately like this:
+// Note: Assigning a new object to `exports` (e.g., `exports = { sum }`) will not work as expected, because it breaks the reference to `module.exports`.
 
-// exports.sum = sum;
+// --- Additional Notes ---
+// - When you import a module, the entire file is executed once, and only the exported parts are accessible.
+// - In ES6 modules, unused code can be eliminated via "tree-shaking", but CommonJS does not support this optimization.
 
-// (V.IMP-NOTE)-[Commonjs-Syntax] :- If we export something (like a function) and import it in another file:then that function will be available in the importing file:
-// V.V.IMP-[NOTE]: And also first that entire file gets executed once from where we are exporting some-thing:
-// V.V.IMP: later the imports use that already executed code. But only the specific parts of that we import will be accessible in the importing file. This is little different in ES6 modules, which can eliminate unused code.because of its (tree-shaking) approach:
-
-// we can see that with the help of (console) statement:
-console.log("executing the hole file");
+// For demonstration, let's log when this file is executed:
+console.log("Executing the entire arithmetic.js file");
--- Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
+++ Lecture2-ModulesInNodesJs/Topic1-UnderstandingModules/code/result.js
@@ -1,75 +1,43 @@
-// here we are gonna use those arithmetical-function modules which we have exported from the arithmetic-file using (commonjs) exported-syntax:and gonna get the result from them and print it on the terminal:
-
-// ===>V.V.IMP: here we gonna learn about that:how be (Import) things in (commonjs) modules syntax: acc to its exports or exporting-ways:
-// V.V.IMP-NOTE = Here In (commonjs):we (Import) things with the help of (require) method:
-// IMP-NOTE: when we say that:we are importing the all modules at once:it basically means that when we import be are importing the hole-file of our modules:but we are only gonna be able to access the modules from that file which we have exported:
-// V.V.IMP(NOTE) = when we (call) or (Import) our (module-functions) which we have created and exported:they are representing there selfs as (properties):because of the module.exports object:because we are exporting those functions through the object:
-
-// => V.IMP = Here through (commonjs) syntax we can import the modules in two-ways:It mostly depend on that how we are exporting things:
-// => IMP-NOTES:basically we have two ways of importing things:and three ways to export things in commonjs syntax:so here we gonna learn about that:how we can use those two ways of (imports) with the three-ways of (exports):
-
-// NOTE: These are the three ways of exporting things:
-// [first-way] = if we export some thing using (objects) with the module.exports keyword:which means that if we are assigning our exported things as (properties) or (key-value) to the objects:then we have to use the (first-way) to import things:
-// [second-way] = if we are directly using them with the (module.exports) keyword then we have to use the (first-way) to import things:only with its (one-type) of export-results we are gonna be able to we use the (second-way) of importing things:  
-// IMP-NOTE:With in second way of exporting things:we get two types of exports:or we can say that we get the two-different types of export-results through the second-way of exporting things:
-// -> 1 = First its exports things like all other-ways:by simply exporting them as (properties) or (key-value) pairs:
-// here we gonna use the (first-way) of importing things same as others:
-// -> 2(V.IMP) = Second it exports thing as they are:[for-example]:if we are exporting the (function):then its gonna be exported them as (function):not as (properties) or (key-value) pairs like we have in in the other-ways:
-// here we gonna use the (second-way) of importing things:and only with this type of (exports) we are gonna be able to use the (second-way) of importing things:
-// [third-way] = if we are directly using them with the (exports) keyword then we also have to use the (first-way) to import things:
-
-// [IMP-NOTE]:- Its basically a simple distribution:To use those two-ways of (imports) on those three-ways of (exports):
-// -> [first-way-export]: if we are using (objects) to export things:or we can say assigning our things as (properties) or (Key-value) pairs to object and then exporting them:In that case we have the (first-way) of importing things:
-// -> [second and third-way-export]: if we exporting things directly by using (module.exports) and (exports) keyword:then we also use the (first-way) to import things:
-// [IMP-NOTE]:because here we are only changing the syntax to export things:but they are gonna be exported through (objects) or as (properties):
-// [V.V.IMP-NOTE]:With in the second-way of exporting things:we have two-type of export-results:so the (second-way) of import things are only gonna be used with the (second-type) of (module.exports) exported-results:which means when we export thing as it is they are not gonna be exported as (properties):
-
-// Learning About Importing-Ways:
-// [first-way] = here we are using the  first-way of importing things:
-// [IMP-NOTE]: It works with the (first),(second) and (third) way of exporting things:
-// => 1(first-type) = Is that we can import the hole-file:and store it in the variable:and from that variable-name we can access our module functions or other things those we have exported:and access them by using the dot (.) keyword on the variable-name:
-
-// const arithmeticModules = require("./arithmetic");
-// console.log(arithmeticModules.add(5, 5));
-
-// => 2(second-type) = Is that we can only import those modules which we want to use from the  exported-file:for doing this basically we need to import the modules using (require) method:and put it in the (object) by using curly braces:
-
-// const { Div, add } = require("./arithmetic");
-// console.log(Div(5, 5));
-// console.log(add(5, 25));
+// Demonstrating how to import and use modules exported from 'arithmetic.js' using CommonJS syntax.
 
 
-// => 1: Checking [first-way] import with [first-way] export:
-// -> [first-type-import]:
-// const arithmeticModules = require("./arithmetic");
-// console.log(arithmeticModules.Add(5, 5));
-// -> [second-type-import]:
-// const { Add, Div } = require("./arithmetic");
-// console.log(Add(5, 5));
-// console.log(Div(5, 5));
+// In Node.js, modules can be imported in different ways depending on how they are exported. 
+// Let's clarify the main approaches:
 
-// => 2: Checking [first-way] import with [second-way] export:
-// -> [first-type-import]:
-// const arithmeticModules = require("./arithmetic");
-// console.log(arithmeticModules.multiple(5, 5));
-// -> [second-type-import]:
-// const { multiple } = require("./arithmetic");
-// console.log(multiple(5, 5));
-// => 3: Checking [first-way] import with [third-way] export:
-// -> [first-type-import]:
-// const arithmeticModules = require("./arithmetic");
-// console.log(arithmeticModules.subtraction(125, 5));
-// -> [second-type-import]:
-// const { subtraction } = require("./arithmetic");
-// console.log(subtraction(125, 5));
+// --- Exporting in CommonJS ---
+// 1. Exporting multiple functions as properties of an object (module.exports = { ... })
+// 2. Exporting a single function directly (module.exports = function ...)
+// 3. Attaching functions as properties to module.exports or exports
 
-// [second-way] = Second-way of Importing things:Only works with the (second-type) exported-result of (module.exports) keywords:
-// That (second-type) exported-result of (module.exports) keywords are those things which we are exporting directly by using (module.exports) keyword:and have also created those things with there (simple-creating) forms:and they are not gonna be exported as (properties) or (key-value) pairs:they are exported in there own forms:
-// IMP: for-example:if we are creating the function using simple (function) keyword or we can say that the way we create the function in simple-form:then we have to use the (second-way) of importing things:
+// --- Importing in CommonJS ---
+// There are two main ways to import modules:
 
-// IMP-NOTE: In second-way of importing things:we basically use the (require) method:To import things:but here In second-way we basically import the things and (store) them in the variables:and use those (variables) directly to access those things:not with the help of (.) dot-keyword as we do in first-way of importing things:
+// [1] Import the entire module as an object, then access exported members as properties:
+    // const arithmetic = require("./arithmetic");
+    // console.log(arithmetic.add(5, 5));
+
+// [2] Destructure specific exports directly (works when module.exports is an object):
+    // const { add, divide } = require("./arithmetic");
+    // console.log(add(5, 10));
+
+// [3] If the module exports a single function directly (module.exports = function...), 
+// you can import it as a function:
+    // const sum = require("./arithmetic");
+    // console.log(sum(34, 5));
+
+// --- Example with the current 'arithmetic.js' ---
+// In 'arithmetic.js', we have:
+// module.exports = function sum(x, y) { return x + y; };
+// So, the module exports a single function directly.
+
+// Therefore, we import it as follows:
 const sum = require("./arithmetic");
-console.log(sum(34,5));
+console.log(sum(34, 5)); // Output: 39
 
-//(IMP-NOTE) = here we can see that:so when we are basically importing the same file again:then it did not gonna work because of nodejs's (module-caching) mechanism: because when we import our files using (commonjs) and (ES6) syntax-methods.then in there first time imports these files basically get stored with in the (cache) mechanism of nodejs:so that's why when we are importing it again it will not gonna work:because system gonna be still have that same-file with in him or in its (caching) mechanism:
+// Note: If you try to import using destructuring or as an object, it will not work in this case,
+// because the module itself is a function, not an object with properties.
+
+// --- Additional Note ---
+// Node.js caches modules after the first require call. 
+// If you require the same module again, you get the cached version, not a fresh instance.
 
