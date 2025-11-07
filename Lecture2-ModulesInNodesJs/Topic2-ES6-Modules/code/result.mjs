2326d64f2d7809b3db722d1b32d8830b7e95e72b
harish kumar <harishpathania150@gmail.com>
2025-11-07 21:25:28 +0530

    revise about commonjs

--- Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/result.mjs
+++ Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/result.mjs
@@ -1,25 +1,33 @@
-// => Here we are going to learn that:how to import things using the ES6 module syntax:
+// --- How to Import Modules Using ES6 Module Syntax in Node.js ---
 
-// IMP:Its very simple:To import things using the (ES6) module syntax:we only have to use the (import) keyword:
+// ES6 modules make it easy to organize and reuse code using the `import` keyword.
+// This file demonstrates two main ways to import modules in Node.js using ES6 syntax (.mjs files).
 
-// IMP-NOTES:
-// [first-note]:when we say that.we are importing the all modules at once:it basically means that we are only importing the modules which we have exported:and not importing any other modules:
-// [second-note]:so when we are use (ES6) module-syntax:then we have to use the (mjs) extension in our file-name:instead of simple (js) extension:as we have learn that otherwise our application code will not gonna get executed:
-// [third-note]:(MOST-IMP):When using ES6 modules, we import modules as functions, classes, or objects, rather than as properties of a single exported object (as is typical with CommonJS). Unlike CommonJS, where all exports are properties of a module object, ES6 modules use (named) or (default) exports for better (tree-shaking) and (static-analysis):
+// IMPORTANT NOTES:
+// 1. With ES6, you can only import what was explicitly exported from a module file. Anything not exported is not accessible.
+// 2. When writing ES6 modules in Node.js, use the `.mjs` file extension for compatibility, unless your project is configured for ESM with `.js` files.
+// 3. ES6 modules support named and default exports, enabling better static analysis and tree-shaking than CommonJS.
 
-// => V.IMP: In (ES6) module syntax:we basically have two ways to import the things:
-// [First-way] = Is that we can import the all modules at once:
-// [Second-way] = Is that we can only import those modules which we want to use from the exported-file:
+// There are two primary styles of ES6 imports:
 
-// [First-way]: We use the asterisk-keyword (*) to import all modules at once:and store them in the (variable-name) with the help of (as) keyword:and from that variable-name we can access our module functions or other things those we have exported:and we can access them from (variable-name) through the help of dot (.) keyword:
-// V.IMP: know here everything means that we are only gonna be able to access the modules from that file which we have exported:
-// V.IMP = all things are gonna we store with in the variable-name (object):the variable-name which we have created  basically to use with in the (import) method:
+// 1. Importing *all* module exports as a single namespace object
+//    - Use `import * as <ObjectName> from "<module>"` syntax.
+//    - All named exports from the target module are grouped as properties of your chosen object.
+//    - Example below: All exports from "script.mjs" will be accessible via the `arithmeticModules` object.
 
 import * as arithmeticModules from "./script.mjs";
-console.log(arithmeticModules.multiple(5, 6));
+// Access via property on namespace:
+console.log(arithmeticModules.multiple(5, 6)); 
 
-// [Second-way]: Here we also use the (import) keyword to import things:but if we only want to import the particular thing from the exported-file:then we have to use the (curly-brackets) or (object) instead of (*) and (as) keywords:and with in those curly-brackets or (object) we basically import our things:
-// V.IMP-NOTE:Here we have to take care of one-thing as well:which is that the (thing) we are importing:that should have the (same-name) as the (name) on which we have exported it:
+
+// 2. Importing only the specific named exports you need
+//    - Use `import { exportName } from "<module>"` syntax.
+//    - This allows you to import just a subset of the exports, reducing clutter and making code more concise.
+//    - You must use the exact export name as specified in the exported module.
 
 import { sum } from "./script.mjs";
 console.log(sum(7, 7));
+
+// In summary:
+// - Use the namespace syntax (`import * as ...`) to access all exports as a bundled object.
+// - Use destructuring import (`import { ... }`) for selective, clean access to specific exports.
--- Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/script.mjs
+++ Lecture2-ModulesInNodesJs/Topic2-ES6-Modules/code/script.mjs
@@ -1,37 +1,36 @@
-// ==> In This:- Topic-file we are gonna be learn about (ES6 Module syntax):
+// In this topic file, we're going to learn about ES6 module syntax in Node.js.
 
-// V.V.IMP = In ES6-modules syntax we basically use the (export) keyword simply to export things or modules we can say from one file to another file:
+// ES6 modules allow us to organize and share code using the modern `import` and `export` keywords,
+// which are supported by most JavaScript environments (including frameworks and libraries).
+// Unlike CommonJS (`module.exports`), ES6 modules don't require creating an exports object: you simply use the `export` keyword before whatever you want to share.
 
-// => ES6 Module:The ES6 module syntax is a more modern approach that is supported by modern JavaScript environments or we can say by the (Libraries) and (Frameworks): and it uses the "import" and "export" keywords:
-// V.IMP = For this approach we did not have to create the module.exports object:we can simply export our function or anything that we want to export as a module by using the (export) keyword in (front) of them or (after) them:
-// V.IMP = we have use this (module) method or syntax with in our (react) learnings:
+// IMPORTANT: When using ES6 module syntax in standalone Node.js projects (not with frameworks/libraries),
+// you should save your modules with the `.mjs` extension instead of `.js`.
+// If you use `.js` without configuring your project to use ES modules, the code might not execute as expected.
 
-// V.V.IMP(NOTE) = so when we are using the (ES6) module-syntax:To (import) and (export) Modules from one file to another file:Mostly when we are creating the application simply using (nodejs) environment or we can say without the help of any (frameworks) and (libraries):then we have to use the (mjs) extension in the file-name:instead of using simple (js) extension:
-// because In this case:if we use the simple (js) extension:then our application code will not gonna get executed:
+// With ES6 modules, you can define and export values (functions, variables, classes, etc.) in multiple-ways:
 
-// V.IMP: So when we are working with the (ES6) module syntax:then we can also create the things in multiple-ways:And also we can also export them in multiple-ways:
+// Example 1: Exporting a function declaration directly with the `export` keyword:
 
-// for-example: if we are creating the (functions):
-// so there are multiple-way to create them and then export them:with the help of (ES6) module syntax:
-// [First-type]: Simple-Functions:means those functions which has been created using the (function) keyword:
-// IMP-NOTE:If we wanna export it:using (ES6) module syntax:then we simply have to use the (export) keyword in front of (function) or (thing) which we wanna exported:
 export function multiple(x, y) {
   return x * y;
 }
 
-// [Second-type]: Arrow-Functions:means those function which we are not created using the (function) keywords:they are created using the (arrow-functions) method:
-// IMP: Same thing we have to do with it:if we want to export it:using (ES6) module syntax:then we simply have to use the (export) keyword in front of (function) or (thing) which we wanna exported:
-// V.IMP-NOTE:Here we have one thing also:if we are using the (arrow-functions) directly with the (export) keyword:then we have to create the arrow-function (variable-name) through the help of (variable-declaration) keyword:like (const) mostly which we use to declare the (variable):
-// because here we did not use the dot (.) keyword to assign the (function) and (things) or we can say there (variable-name) to the (exported) keyword:like we did in the (commonjs) syntax:
-
+// Example 2: Exporting arrow functions as named exports.
+// Remember to declare the function as a variable (typically with `const`) and add the `export` keyword.
 export const sum = (x, y) => {
   return x + y;
 };
-export const div= (x, y) => {
+
+export const div = (x, y) => {
   return x / y;
 };
 
-// (IMP-NOTE) = When we export something (like a function) from one file and import it in another, the entire file with the export runs once when it’s imported. After that, any further imports use the already executed file:
-// However, only the parts we specifically import are accessible in the importing file:which we are basically exporting from one file to another file:
-// IMP-NOTE: In ES6 modules, this process is slightly different because of tree-shaking approach. Tree-shaking removes unused code, so only the parts that are actually used are included in the final output, making the code smaller and more efficient.
-// [Tree shaking]: It's a term used in JavaScript for dead code elimination. It's a process that removes unused code from your final bundle, resulting in smaller and faster applications. Tree shaking relies heavily on the static structure of ES6 modules (import and export statements). This allows bundlers to analyze your code and determine which parts are actually used:
+// NOTES:
+// - When you import something from an ES6 module, the entire file is executed once.
+//   After that, additional imports use the already loaded (cached) module.
+// - Only the parts you explicitly import (the exported members) are accessible in your importing file.
+
+// ADVANTAGE: One major benefit of ES6 modules is "tree-shaking":
+// - Tree-shaking is the process of removing unused code from the final bundle.
+// - Because ES6 module imports/exports are statically analyzable, bundlers can detect what code isn't used,producing smaller and more efficient applications by eliminating dead (unused) code.
