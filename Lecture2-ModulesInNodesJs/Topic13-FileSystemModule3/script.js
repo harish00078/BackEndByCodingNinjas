707a6d4c20f422352e5980249a5235a95e5584e6
harish kumar <harishpathania150@gmail.com>
2025-11-16 10:11:42 +0530

    revise about file system module async-methods

--- Lecture2-ModulesInNodesJs/Topic13-FileSystemModule3/script.js
+++ Lecture2-ModulesInNodesJs/Topic13-FileSystemModule3/script.js
@@ -1,110 +1,97 @@
-console.log("learning file system module in node js");
-// Here we are going to learn about (Asynchronous-Methods) of file-system-module in node js:
+/**
+ * Demonstration of asynchronous CRUD operations using Node.js File System (fs) module.
+ * 
+ * Asynchronous methods in the fs module take a callback function as their last argument.
+ * These methods are non-blocking, which means Node.js can handle other tasks while waiting for
+ * file operations to complete. Prefer them over synchronous (blocking) methods for production.
+ */
 
-// V.IMP:the (Asynchronous) methods basically did not have the (async) keyword at the last of the method-name:the way we have with in the synchronous-methods:and we did not have do that because the (asynchronous) methods are (default) in the node js:and we can use them without using the (async) keyword at the last of the method-name:
-
-// we gonna learn about Asynchronous-methods of fS-module by performing CRUD-operations on a files same as we have learned with in the (synchronous-methods) of fs-module:
-
-// -> first import module:
 const fs = require("fs");
 
-// V.IMP-NOTE(working):In (Asynchronous) methods:we basically have the (callback) function as an (second or third-argument):as we have learn with in the working-system of (Asynchronous) in js:
-// -> 1 =  this difference between (synchronous) and (asynchronous) method:In (synchronous) methods we basically provide the (data) as an (argument/parameter) directly:
-// -> 2 =In asynchronous methods, data is not provided directly as an argument to file system (fs) methods. Instead, to handle asynchronous operations, other methods or mechanisms are used to manage these processes effectively:these methods basically help us to achieve the (asynchronous) working of js:
-// -> those other methods or mechanisms basically are (callback-function) or other-methods and ways which basically help us to handle or perform the (asynchronous-operations):
+// console.log("Learning File System Module in Node.js (Asynchronous Methods)");
 
+// // 1. READ a file (fs.readFile)
+// // - Reads the contents of the file asynchronously.
+// // - Arguments: file path, encoding, callback(error, data)
+// fs.readFile("./data.txt", "utf-8", (err, data) => {
+//     if (err) {
+//         console.error("Error while reading the file:", err.message);
+//     } else {
+//         console.log("Data from data.txt:");
+//         console.log(data);
+//     }
+// });
 
-// V.IMP-NOTE:here we are using the asynchronous-methods:so to handle the (asynchronous-methods):we have to provide them the (callback-function) as an (argument):as we have learned that in asynchronous-system working in js:
-// V.IMP:these methods mostly takes a callback function as their last- arguments:which is called when the operation is completed. The callback function has (two-arguments):
-// -> 1 = the first argument is an (error-object):
-// -> 2 = the second argument is the (result) of the operation:
-// IMP-NOTE: If an error occurred, the first argument will contain an error object, otherwise, the first argument will be null:The second argument will contain the result of the operation:
+// // 2. CREATE (write) a new file (fs.writeFile)
+// // - Overwrites file if it exists, otherwise creates a new one.
+// // - Arguments: file path, data to write, callback(error)
+// fs.writeFile("./data2.txt", "name: Harish\nage: 24\n", (err) => {
+//     if (err) {
+//         console.error("Error while creating/writing to data2.txt:", err.message);
+//     } else {
+//         console.log("data2.txt has been created or overwritten.");
 
+//         // Read newly created data2.txt to verify
+//         fs.readFile("./data2.txt", "utf-8", (readErr, readData) => {
+//             if (readErr) {
+//                 console.error("Error while reading data2.txt after writing:", readErr.message);
+//             } else {
+//                 console.log("Contents of data2.txt after creation:");
+//                 console.log(readData);
+//             }
+//         });
+//     }
+// });
 
-// V.IMP-NOTE: As we have learned with in the (Asynchronous)  or (event-loop) architecture in js: The (Asynchronous) methods are non-blocking, which means that the main-thread will not be blocked, and it can perform other tasks while the file operation is being performed. The (Asynchronous) methods are always preferred over the blocking methods:and as we have learned that (asynchronous) working of (js):Can be achieved with the help of (callbacks) or other-methods and mechanisms which help us to handle the (asynchronous-operations) of js:
+// // 3. UPDATE (append) data to a file (fs.appendFile)
+// // - Appends data to the end of the file.
+// // - Arguments: file path, data to append, callback(error)
+// fs.appendFile("./data2.txt", "position: software developer\n", (err) => {
+//     if (err) {
+//         console.error("Error while updating/appending to data2.txt:", err.message);
+//     } else {
+//         console.log("data2.txt has been updated (appended).");
 
+//         // Read updated data2.txt to verify
+//         fs.readFile("./data2.txt", "utf-8", (readErr, readData) => {
+//             if (readErr) {
+//                 console.error("Error while reading updated data2.txt:", readErr.message);
+//             } else {
+//                 console.log("Contents of data2.txt after update:");
+//                 console.log(readData);
+//             }
+//         });
+//     }
+// });
 
+// // 4. DELETE a file (fs.unlink)
+// // - Deletes the specified file asynchronously.
+// // - Arguments: file path, callback(error)
+// fs.unlink("./data2.txt", (err) => {
+//     if (err) {
+//         console.error("Error while deleting data2.txt:", err.message);
+//     } else {
+//         console.log("data2.txt deleted successfully.");
+//         // Attempt to read after deletion to show result
+//         fs.readFile("./data2.txt", "utf-8", (readErr, data) => {
+//             if (readErr) {
+//                 console.error("data2.txt no longer exists (as expected):", readErr.message);
+//             } else {
+//                 // Should not reach here
+//                 console.log("Contents of data2.txt after supposed deletion:", data);
+//             }
+//         });
+//     }
+// });
 
-// -> learn to use Asynchronous-methods of Fs-module to perform CRUD-operations on a file:
-
-// 1. Reading a file:
-// We have the (readFile) method:It is used to read the data from a file:
-// IMP-NOTE: The (readFile) method takes three arguments:
-// -> 1 =  the (path) of the file:which we want to read:
-// -> 2 = the (encoding) of the file:convert the buffer-data into a string-data:
-// -> 3 = the (callback-function):which is called when the operation is completed:
-// -> the callback-function has two arguments:
-// -> 1 = the first argument is an (error-object):
-// -> 2 = the second argument is the result of the operation:
-
-// -> first-way:with the help of (arrow-function):
-// const readingFile = fs.readFile("data.txt","utf-8",(err,data)=>{});
-// -> second-way:with the help of (normal-function):
-const readingFile = fs.readFile("./data.txt","utf-8",function(err,data){
-    // here first check if an error occurred or not:
-    if(err){
-        console.log('error while reading the file:',err);
-    }else{
-        // if no error occurred then print the data:
-        console.log('data from the file:',data);
+const studentData = fs.readFile('./data.txt','utf-8',dataHandle());
+function dataHandle(){
+   return (err,data)=>{
+        if(err){
+            console.error("Error while reading the file:", err.message);
+        }else{
+            console.log("Data from data.txt:");
+            console.log(data);
+        }
     }
-});
-
-
-// 2. Create a file:
-// we have the (writeFile) method:It is used to write the data into a file:or create a new file if it does not exist:
-// IMP-NOTE: The (writeFile) method mostly takes three arguments:
-// -> 1 = the (path) of the file:which we want to create:
-// -> 2 = the (data) that we want to write to the file:
-// -> 3 = the (callback-function):which is called when the operation is completed:
-// -> the callback-function has one arguments/parameter:
-// -> 1 = the argument is an (error) or (error-object):if an error occurred, the argument will contain an (error-object), otherwise, the argument will be null:
-const creatingFile = fs.writeFile("./data2.txt","name:harish,age:24,",function(err){
-    // CHECKING-ERROR:
-    if(err){
-        console.log('error while creating the file:',err);
-    }
-})
-
-const readingFile2 = fs.readFile("./data2.txt","utf-8",function(err,data){
-    // here first check if an error occurred or not:
-    if(err){
-        console.log('error while reading the file:',err);    
-    }else{
-        // if no error occurred then print the data:
-        console.log('data from data2-file:',data);
-    }
-})
-
-// 3. Update a file:
-// we have the (appendFile) method:It is used to append or update the data into a file:
-// IMP-NOTE: The (appendFile) method mostly takes three arguments:
-// -> 1 = the (path) of the file:which we want to update:
-// -> 2 = the (data) that we want to append to the file:
-// -> 3 = the (callback-function):which is called when the operation is completed:
-// -> the callback-function has one arguments/parameter:
-// -> 1 = the argument is an (error) or (error-object):if an error occurred, the argument will contain an (error-object), otherwise, the argument will be null:
-const updatingFile = fs.appendFile("./data2.txt","position:software developer",function(err){
-    // CHECKING-ERROR:
-    if(err){
-        console.log('error while updating the file:',err);
-    }else{
-        console.log('file updated successfully');
-    }
-});
-
-// 4. Delete a file:
-// we have the (unlink) method:It is used to delete a file:
-// IMP-NOTE: The (unlink) method mostly takes two arguments:
-// -> 1 = the (path) of the file:which we want to delete:   
-// -> 2 = the (callback-function):which is called when the operation is completed:
-// -> the callback-function has one arguments/parameter:
-// -> 1 = the argument is an (error) or (error-object):if an error occurred, the argument will contain an (error-object), otherwise, the argument will be null:
-const deletingFile = fs.unlink("./data2.txt",function(err){
-    // CHECKING-ERROR:
-    if(err){
-        console.log('error while deleting the file:',err);
-    }else{
-        console.log('file deleted successfully');
-    }
-})
+}
